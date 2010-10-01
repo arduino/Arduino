c69279331f3b90d34945cc365b399881f194ed2c
Mark Sproul <msproul@tiger3.engr.rutgers.edu>
2010-10-01 10:25:06 -0400

    changed from cpu #ifdefs to register #ifdefs

--- hardware/arduino/cores/arduino/wiring_analog.c
+++ hardware/arduino/cores/arduino/wiring_analog.c
@@ -83,17 +83,24 @@ int analogRead(uint8_t pin)
 	// and ADCH until ADCH is read.  reading ADCL second would
 	// cause the results of each conversion to be discarded,
 	// as ADCL and ADCH would be locked when it completed.
-	low = ADCL;
-	high = ADCH;
+	low		=	ADCL;
+	high	=	ADCH;
+#else
+	//*	we dont have an ADC, return 0
+	low		=	0;
+	high	=	0;
+#endif
 
 	// combine the two bytes
 	return (high << 8) | low;
 }
 
+//*********************************************************************
 // Right now, PWM output only works on the pins with
 // hardware support.  These are defined in the appropriate
 // pins_*.c file.  For the rest of the pins, we default
 // to digital output.
+//*********************************************************************
 void analogWrite(uint8_t pin, int val)
 {
 	// We need to make sure the PWM output is enabled for those pins
@@ -102,98 +109,166 @@ void analogWrite(uint8_t pin, int val)
 	// for consistenty with Wiring, which doesn't require a pinMode
 	// call for the analog output pins.
 	pinMode(pin, OUTPUT);
-	
-	if (digitalPinToTimer(pin) == TIMER1A) {
-		// connect pwm to pin on timer 1, channel A
-		sbi(TCCR1A, COM1A1);
-		// set pwm duty
-		OCR1A = val;
-	} else if (digitalPinToTimer(pin) == TIMER1B) {
-		// connect pwm to pin on timer 1, channel B
-		sbi(TCCR1A, COM1B1);
-		// set pwm duty
-		OCR1B = val;
-#if defined(__AVR_ATmega8__)
-	} else if (digitalPinToTimer(pin) == TIMER2) {
-		// connect pwm to pin on timer 2, channel B
-		sbi(TCCR2, COM21);
-		// set pwm duty
-		OCR2 = val;
-#else
-	} else if (digitalPinToTimer(pin) == TIMER0A) {
-		if (val == 0) {
-			digitalWrite(pin, LOW);
-		} else {
-			// connect pwm to pin on timer 0, channel A
-			sbi(TCCR0A, COM0A1);
-			// set pwm duty
-			OCR0A = val;      
-		}
-	} else if (digitalPinToTimer(pin) == TIMER0B) {
-		if (val == 0) {
-			digitalWrite(pin, LOW);
-		} else {
-			// connect pwm to pin on timer 0, channel B
-			sbi(TCCR0A, COM0B1);
-			// set pwm duty
-			OCR0B = val;
-		}
-	} else if (digitalPinToTimer(pin) == TIMER2A) {
-		// connect pwm to pin on timer 2, channel A
-		sbi(TCCR2A, COM2A1);
-		// set pwm duty
-		OCR2A = val;	
-	} else if (digitalPinToTimer(pin) == TIMER2B) {
-		// connect pwm to pin on timer 2, channel B
-		sbi(TCCR2A, COM2B1);
-		// set pwm duty
-		OCR2B = val;
-#endif
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-	// XXX: need to handle other timers here
-	} else if (digitalPinToTimer(pin) == TIMER3A) {
-		// connect pwm to pin on timer 3, channel A
-		sbi(TCCR3A, COM3A1);
-		// set pwm duty
-		OCR3A = val;
-	} else if (digitalPinToTimer(pin) == TIMER3B) {
-		// connect pwm to pin on timer 3, channel B
-		sbi(TCCR3A, COM3B1);
-		// set pwm duty
-		OCR3B = val;
-	} else if (digitalPinToTimer(pin) == TIMER3C) {
-		// connect pwm to pin on timer 3, channel C
-		sbi(TCCR3A, COM3C1);
-		// set pwm duty
-		OCR3C = val;
-	} else if (digitalPinToTimer(pin) == TIMER4A) {
-		// connect pwm to pin on timer 4, channel A
-		sbi(TCCR4A, COM4A1);
-		// set pwm duty
-		OCR4A = val;
-	} else if (digitalPinToTimer(pin) == TIMER4B) {
-		// connect pwm to pin on timer 4, channel B
-		sbi(TCCR4A, COM4B1);
-		// set pwm duty
-		OCR4B = val;
-	} else if (digitalPinToTimer(pin) == TIMER4C) {
-		// connect pwm to pin on timer 4, channel C
-		sbi(TCCR4A, COM4C1);
-		// set pwm duty
-		OCR4C = val;
-	} else if (digitalPinToTimer(pin) == TIMER5A) {
-		// connect pwm to pin on timer 5, channel A
-		sbi(TCCR5A, COM5A1);
-		// set pwm duty
-		OCR5A = val;
-	} else if (digitalPinToTimer(pin) == TIMER5B) {
-		// connect pwm to pin on timer 5, channel B
-		sbi(TCCR5A, COM5B1);
-		// set pwm duty
-		OCR5B = val;
-#endif
-	} else if (val < 128)
+	if (val == 0)
+	{
 		digitalWrite(pin, LOW);
-	else
+	}
+	else if (val == 255)
+	{
 		digitalWrite(pin, HIGH);
+	}
+	else
+	{
+		switch(digitalPinToTimer(pin))
+		{
+		#if defined(TCCR0) && defined(COM00) && !defined(__AVR_ATmega8__)
+		//*	I need to fix this for atmega8
+			case TIMER0A:
+				// connect pwm to pin on timer 0
+				sbi(TCCR0, COM00);
+				OCR0	=	val;				// set pwm duty
+				break;
+		#endif
+
+		#if defined(TCCR0A) && defined(COM0A1)
+			case TIMER0A:
+				// connect pwm to pin on timer 0, channel A
+				sbi(TCCR0A, COM0A1);
+				OCR0A	=	val;      			// set pwm duty
+				break;
+		#endif
+
+		#if defined(TCCR0A) && defined(COM0B1)
+			case TIMER0B:
+				// connect pwm to pin on timer 0, channel B
+				sbi(TCCR0A, COM0B1);
+				OCR0B	=	val;				// set pwm duty
+				break;
+		#endif
+
+		#if defined(TCCR1A) && defined(COM1A1)
+			case TIMER1A:
+				// connect pwm to pin on timer 1, channel A
+				sbi(TCCR1A, COM1A1);
+				OCR1A	=	val;				// set pwm duty
+				break;
+		#endif
+
+		#if defined(TCCR1A) && defined(COM1B1)
+			case TIMER1B:
+				// connect pwm to pin on timer 1, channel B
+				sbi(TCCR1A, COM1B1);
+				OCR1B	=	val;				// set pwm duty
+				break;
+		#endif
+
+		#if defined(TCCR2) && defined(COM21)
+			case TIMER2:
+				// connect pwm to pin on timer 2, channel B
+				sbi(TCCR2, COM21);
+				OCR2	=	val;				// set pwm duty
+				break;
+		#endif
+
+		#if defined(TCCR2A) && defined(COM2A1)
+			case TIMER2A:
+				// connect pwm to pin on timer 2, channel A
+				sbi(TCCR2A, COM2A1);
+				OCR2A	=	val;				// set pwm duty
+				break;
+		#endif
+
+		#if defined(TCCR2A) && defined(COM2B1)
+			case TIMER2B:
+				// connect pwm to pin on timer 2, channel B
+				sbi(TCCR2A, COM2B1);
+				OCR2B	=	val;				// set pwm duty
+				break;
+		#endif
+
+		#if defined(TCCR3A) && defined(COM3A1)
+			case TIMER3A:
+				// connect pwm to pin on timer 3, channel A
+				sbi(TCCR3A, COM3A1);
+				OCR3A	=	val;				// set pwm duty
+				break;
+		#endif
+
+		#if defined(TCCR3A) && defined(COM3B1)
+			case TIMER3B:
+				// connect pwm to pin on timer 3, channel B
+				sbi(TCCR3A, COM3B1);
+				OCR3B	=	val;				// set pwm duty
+				break;
+		#endif
+
+		#if defined(TCCR3A) && defined(COM3C1)
+			case TIMER3C:
+				// connect pwm to pin on timer 3, channel C
+				sbi(TCCR3A, COM3C1);
+				OCR3C	=	val;				// set pwm duty
+				break;
+		#endif
+
+		#if defined(TCCR4A) && defined(COM4A1)
+			case TIMER4A:
+				// connect pwm to pin on timer 4, channel A
+				sbi(TCCR4A, COM4A1);
+				OCR4A	=	val;				// set pwm duty
+				break;
+		#endif
+
+		#if defined(TCCR4A) && defined(COM4B1)
+			case TIMER4B:
+				// connect pwm to pin on timer 4, channel B
+				sbi(TCCR4A, COM4B1);
+				OCR4B	=	val;				// set pwm duty
+				break;
+		#endif
+
+		#if defined(TCCR4A) && defined(COM4C1)
+			case TIMER4C:
+				// connect pwm to pin on timer 4, channel C
+				sbi(TCCR4A, COM4C1);
+				OCR4C	=	val;				// set pwm duty
+				break;
+		#endif
+
+		#if defined(TCCR5A) && defined(COM5A1)
+			case TIMER5A:
+				// connect pwm to pin on timer 5, channel A
+				sbi(TCCR5A, COM5A1);
+				OCR5A	=	val;				// set pwm duty
+				break;
+		#endif
+
+		#if defined(TCCR5A) && defined(COM5B1)
+			case TIMER5B:
+				// connect pwm to pin on timer 5, channel B
+				sbi(TCCR5A, COM5B1);
+				OCR5B	=	val;				// set pwm duty
+				break;
+		#endif
+
+		//*	Jul 27, 2010	<MLS> Added this, Its missing from the main core files
+		#if defined(TCCR5A) && defined(COM5C1)
+			case TIMER5C:
+				// connect pwm to pin on timer 5, channel C
+				sbi(TCCR5A, COM5C1);
+				OCR5C	=	val;				// set pwm duty
+				break;
+		#endif
+
+			case NOT_ON_TIMER:
+			default:
+				if (val < 128)
+				{
+					digitalWrite(pin, LOW);
+				}
+				else
+				{
+					digitalWrite(pin, HIGH);
+				}
+		}
+	}
 }
