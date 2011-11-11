26ef10db1f5194f66cf2e276adbec246a5762b4c
edbaafi <ed@modk.it>
2011-11-10 23:05:12 -0500

    Added twi_enableGenCall.  Added flag for gcall receive to pass to caller through onSlaveReceive.

--- libraries/Wire/utility/twi.h
+++ libraries/Wire/utility/twi.h
@@ -40,10 +40,11 @@
   
   void twi_init(void);
   void twi_setAddress(uint8_t);
+  void twi_enableGenCall(void);
   uint8_t twi_readFrom(uint8_t, uint8_t*, uint8_t);
   uint8_t twi_writeTo(uint8_t, uint8_t*, uint8_t, uint8_t);
   uint8_t twi_transmit(const uint8_t*, uint8_t);
-  void twi_attachSlaveRxEvent( void (*)(uint8_t*, int) );
+  void twi_attachSlaveRxEvent( void (*)(uint8_t*, int, uint8_t) );
   void twi_attachSlaveTxEvent( void (*)(void) );
   void twi_reply(uint8_t);
   void twi_stop(void);
