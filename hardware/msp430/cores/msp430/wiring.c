75019c9e294fb47611853a8fadf4156c1b6d5010
Robert Wessels <robertinant@yahoo.com>
2012-04-02 10:27:26 -0700

    Add delayMicroseconds()

--- hardware/msp430/cores/msp430/wiring.c
+++ hardware/msp430/cores/msp430/wiring.c
@@ -72,7 +72,7 @@ void initClocks(void)
 	BCSCTL2 &= ~(DIVS_0);
 }
 
-#define SMCLK_FREQUENCY      1000000
+#define SMCLK_FREQUENCY      16000000
 #define WDT_DIVIDER        512
 
 const uint32_t WDT_FREQUENCY = SMCLK_FREQUENCY / WDT_DIVIDER;
@@ -81,6 +81,72 @@ volatile uint32_t wdtCounter = 0;
 unsigned long millis(){
 	return wdtCounter / (WDT_FREQUENCY / 1000);
 }
+
+/* Delay for the given number of microseconds.  Assumes a 1, 8 or 16 MHz clock. */
+void delayMicroseconds(unsigned int us)
+{
+#if F_CPU >= 20000000L
+	// for a one-microsecond delay, simply wait 2 cycle and return. The overhead
+	// of the function call yields a delay of exactly a one microsecond.
+	__asm__ __volatile__ (
+		"nop" "\n\t"
+		"nop"); //just waiting 2 cycle
+	if (--us == 0)
+		return;
+
+	// the following loop takes a 1/5 of a microsecond (4 cycles)
+	// per iteration, so execute it five times for each microsecond of
+	// delay requested.
+	us = (us<<2) + us; // x5 us
+
+	// account for the time taken in the preceeding commands.
+	us -= 2;
+
+#elif F_CPU >= 16000000L
+	// for the 16 MHz clock on most Arduino boards
+
+	// for a one-microsecond delay, simply return.  the overhead
+	// of the function call yields a delay of approximately 1 1/8 us.
+	if (--us == 0)
+		return;
+
+	// the following loop takes a quarter of a microsecond (4 cycles)
+	// per iteration, so execute it four times for each microsecond of
+	// delay requested.
+	us <<= 2;
+
+	// account for the time taken in the preceeding commands.
+	us -= 2;
+#else
+	// for the 1 MHz
+
+	// for a one- or two-microsecond delay, simply return.  the overhead of
+	// the function calls takes more than two microseconds.  can't just
+	// subtract two, since us is unsigned; we'd overflow.
+	if (--us == 0)
+		return;
+	if (--us == 0)
+		return;
+
+	// the following loop takes 4 microsecond (4 cycles)
+	// per iteration, so execute it ones for each 4 microsecond of
+	// delay requested.
+	us >>= 2;
+    
+	// partially compensate for the time taken by the preceeding commands.
+	// we can't subtract any more than this or we'd overflow w/ small delays.
+	us--;
+#endif
+
+	// busy wait
+        __asm__ __volatile__ (
+                "L1: nop \n\t"                  // even steven
+                "dec.w %[us] \n\t"                // 1 instruction
+                "jnz L1 \n\t"                   // 2 instruction
+                : [us] "=r" (us) : "[us]" (us)  // 
+        );
+}
+
 
 // (ab)use the WDT
 void delay(uint32_t milliseconds){
