d35d13730cf5d8cf2643eec9a1772b5f50d450d3
Manfred Jonsson <manfred@gemini.(none)>
2012-05-28 16:02:17 +0200

    Minimum set of oop extensions

--- libraries/LiquidCrystal/LiquidCrystal.cpp
+++ libraries/LiquidCrystal/LiquidCrystal.cpp
@@ -24,6 +24,11 @@
 // can't assume that its in that state when a sketch starts (and the
 // LiquidCrystal constructor is called).
 
+LiquidCrystal::LiquidCrystal()
+{
+  // Not used but makes C++ happy when inherited
+}
+
 LiquidCrystal::LiquidCrystal(uint8_t rs, uint8_t rw, uint8_t enable,
 			     uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
 			     uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7)
--- libraries/LiquidCrystal/LiquidCrystal.h
+++ libraries/LiquidCrystal/LiquidCrystal.h
@@ -59,7 +59,7 @@ public:
 	    uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
 	    uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7);
     
-  void begin(uint8_t cols, uint8_t rows, uint8_t charsize = LCD_5x8DOTS);
+  virtual void begin(uint8_t cols, uint8_t rows, uint8_t charsize = LCD_5x8DOTS);
 
   void clear();
   void home();
@@ -80,14 +80,16 @@ public:
   void createChar(uint8_t, uint8_t[]);
   void setCursor(uint8_t, uint8_t); 
   virtual size_t write(uint8_t);
-  void command(uint8_t);
+  virtual void command(uint8_t);
   
   using Print::write;
-private:
-  void send(uint8_t, uint8_t);
-  void write4bits(uint8_t);
-  void write8bits(uint8_t);
-  void pulseEnable();
+protected:
+  LiquidCrystal();
+
+  virtual void send(uint8_t, uint8_t);
+  virtual void write4bits(uint8_t);
+  virtual void write8bits(uint8_t);
+  virtual void pulseEnable();
 
   uint8_t _rs_pin; // LOW: command.  HIGH: character.
   uint8_t _rw_pin; // LOW: write to LCD.  HIGH: read from LCD.
