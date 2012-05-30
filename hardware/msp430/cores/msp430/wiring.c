e751f2f2f587f3e0babf1d0e0c18d1b1313989c6
Rei Vilo <rei_vilo@yahoo.fr>
2012-05-30 11:47:15 +0200

    Added micros() — Issue #58

--- hardware/msp430/cores/msp430/wiring.c
+++ hardware/msp430/cores/msp430/wiring.c
@@ -1,33 +1,33 @@
 /*
-  ************************************************************************
-  *	wiring.c
-  *
-  *	Arduino core files for MSP430
-  *		Copyright (c) 2012 Robert Wessels. All right reserved.
-  *
-  *
-  ***********************************************************************
-  Derived from:
-  wiring.c - Partial implementation of the Wiring API for the ATmega8.
-  Part of Arduino - http://www.arduino.cc/
-
-  Copyright (c) 2005-2006 David A. Mellis
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General
-  Public License along with this library; if not, write to the
-  Free Software Foundation, Inc., 59 Temple Place, Suite 330,
-  Boston, MA  02111-1307  USA
-*/
+ ************************************************************************
+ *	wiring.c
+ *
+ *	Arduino core files for MSP430
+ *		Copyright (c) 2012 Robert Wessels. All right reserved.
+ *
+ *
+ ***********************************************************************
+ Derived from:
+ wiring.c - Partial implementation of the Wiring API for the ATmega8.
+ Part of Arduino - http://www.arduino.cc/
+ 
+ Copyright (c) 2005-2006 David A. Mellis
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General
+ Public License along with this library; if not, write to the
+ Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ Boston, MA  02111-1307  USA
+ */
 #include "Energia.h"
 
 void initClocks(void);
@@ -36,15 +36,15 @@ void disableWatchDog(void);
 
 void init()
 {
-        disableWatchDog();
+    disableWatchDog();
 	initClocks();
-        enableWatchDogIntervalMode();
-        __eint();
+    enableWatchDogIntervalMode();
+    __eint();
 }
 
 void disableWatchDog(void)
 {
-        /* Diable watchdog timer */
+    /* Diable watchdog timer */
 	WDTCTL = WDTPW | WDTHOLD;
 }
 
@@ -53,7 +53,7 @@ void enableWatchDogIntervalMode(void)
 	/* WDT Password + WDT interval mode + Watchdog clock source /512 + source from SMCLK
 	 * Note that we WDT is running in interval mode. WDT will not trigger a reset on expire in this mode. */
 	WDTCTL = WDTPW + WDTTMSEL + WDTIS1;
- 
+    
 	/* WDT interrupt enable */
 	IE1 |= WDTIE;
 }
@@ -73,12 +73,12 @@ void initClocks(void)
 	BCSCTL1 = CALBC1_1MHZ;
 	DCOCTL = CALDCO_1MHZ;
 #else
-        #warning No Suitable Frequency found!
+#warning No Suitable Frequency found!
 #endif
 	/* SMCLK = DCO / DIVS = nMHz */
 	BCSCTL2 &= ~(DIVS_0);
 	/* ACLK = VLO = ~ 12 KHz */
-        BCSCTL3 |= LFXT1S_2; 
+    BCSCTL3 |= LFXT1S_2; 
 }
 
 #define SMCLK_FREQUENCY F_CPU
@@ -87,9 +87,14 @@ void initClocks(void)
 const uint32_t WDT_FREQUENCY = SMCLK_FREQUENCY / WDT_DIVIDER;
 volatile uint32_t wdtCounter = 0;
 
+unsigned long micros()
+{
+    return (1000 * wdtCounter) / (WDT_FREQUENCY / 1000);
+}
+
 unsigned long millis()
 {
-        return wdtCounter / (WDT_FREQUENCY / 1000);
+    return wdtCounter / (WDT_FREQUENCY / 1000);
 }
 
 /* Delay for the given number of microseconds.  Assumes a 1, 8 or 16 MHz clock. */
@@ -99,37 +104,37 @@ void delayMicroseconds(unsigned int us)
 	/* For a one-microsecond delay, simply wait 2 cycle and return. The overhead
 	 * of the function call yields a delay of exactly one microsecond. */
 	__asm__ __volatile__ (
-		"nop" "\n\t"
-		"nop");
+                          "nop" "\n\t"
+                          "nop");
 	if (--us == 0)
 		return;
-
+    
 	/* The following loop takes a 1/5 of a microsecond (4 cycles)
 	 * per iteration, so execute it five times for each microsecond of
 	 * delay requested. */
 	us = (us<<2) + us; // x5 us
-
+    
 	/* Account for the time taken in the preceeding commands. */
 	us -= 2;
-
+    
 #elif F_CPU >= 16000000L
 	/* For the 16 MHz clock on most boards */
-
+    
 	/* For a one-microsecond delay, simply return.  the overhead
 	 * of the function call yields a delay of approximately 1 1/8 us. */
 	if (--us == 0)
 		return;
-
+    
 	/* The following loop takes a quarter of a microsecond (4 cycles)
 	 * per iteration, so execute it four times for each microsecond of
 	 * delay requested. */
 	us <<= 2;
-
+    
 	/* Account for the time taken in the preceeding commands. */
 	us -= 2;
 #else
 	/* For the 1 MHz */
-
+    
 	/* For a one- or two-microsecond delay, simply return.  the overhead of
 	 * the function calls takes more than two microseconds.  can't just
 	 * subtract two, since us is unsigned; we'd overflow. */
@@ -137,42 +142,42 @@ void delayMicroseconds(unsigned int us)
 		return;
 	if (--us == 0)
 		return;
-
+    
 	/* The following loop takes 4 microsecond (4 cycles)
 	 * per iteration, so execute it ones for each 4 microsecond of
 	 * delay requested. */
 	us >>= 2;
-
+    
 	/* Partially compensate for the time taken by the preceeding commands.
 	 * we can't subtract any more than this or we'd overflow w/ small delays. */
 	us--;
 #endif
-
+    
 	/* Busy wait */
-        __asm__ __volatile__ (
-                /* even steven */
-                "L1: nop \n\t"   
-                /* 1 instruction */
-                "dec.w %[us] \n\t"
-                /* 2 instructions */
-                "jnz L1 \n\t"
-                : [us] "=r" (us) : "[us]" (us)
-        );
+    __asm__ __volatile__ (
+                          /* even steven */
+                          "L1: nop \n\t"   
+                          /* 1 instruction */
+                          "dec.w %[us] \n\t"
+                          /* 2 instructions */
+                          "jnz L1 \n\t"
+                          : [us] "=r" (us) : "[us]" (us)
+                          );
 }
 
 /* (ab)use the WDT */
 void delay(uint32_t milliseconds)
 {
 	uint32_t wakeTime = wdtCounter + (milliseconds * (WDT_FREQUENCY / 1000));
-        while(wdtCounter < wakeTime)
-                /* Wait for WDT interrupt in LMP0 */
-                __bis_status_register(LPM0_bits+GIE);
+    while(wdtCounter < wakeTime)
+    /* Wait for WDT interrupt in LMP0 */
+        __bis_status_register(LPM0_bits+GIE);
 }
 
 __attribute__((interrupt(WDT_VECTOR)))
 void watchdog_isr (void)
 {
-        wdtCounter++;
-        /* Exit from LMP3 on reti (this includes LMP0) */
-        __bic_status_register_on_exit(LPM3_bits);
+    wdtCounter++;
+    /* Exit from LMP3 on reti (this includes LMP0) */
+    __bic_status_register_on_exit(LPM3_bits);
 }
