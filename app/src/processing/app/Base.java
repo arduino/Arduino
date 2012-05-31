207b7e139e77651f8c17cc31ebf5237f364c10e7
Robert Wessels <robertinant@yahoo.com>
2012-05-31 15:39:20 -0700

    Fix for Issue #62. Change -DARUDUINO to -DENERGIA. Change Base.REVISION to 6.

--- app/src/processing/app/Base.java
+++ app/src/processing/app/Base.java
@@ -42,7 +42,7 @@ import static processing.app.I18n._;
  * files and images, etc) that comes from that.
  */
 public class Base {
-  public static final int REVISION = 101;
+  public static final int REVISION = 6;
   /** This might be replaced by main() if there's a lib/version.txt file. */
   static String VERSION_NAME = "0101E0006";
   /** Set true if this a proper release rather than a numbered revision. */
