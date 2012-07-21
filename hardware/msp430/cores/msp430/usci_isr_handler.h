9453df550d825da8dcd832f5883d0630e6a003c2
Robert Wessels <robertinant@yahoo.com>
2012-07-21 16:36:44 -0700

    WIP: - Add USCI shared ISR infrastructure - Fix master reader UART combination - Some cosmetic fixes

---
+++ hardware/msp430/cores/msp430/usci_isr_handler.h
@@ -0,0 +1,18 @@
+#ifndef usci_isr_handler_h
+#define usci_isr_handler_h
+
+#ifdef __MSP430_HAS_USCI__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void uart_tx_isr(void);
+void uart_rx_isr(void);
+void i2c_txrx_isr(void);
+void i2c_state_isr(void);
+void usci_isr_install(void);
+#ifdef __cplusplus
+}
+#endif
+#endif /* __MSP430_HAS_USCI__ */
+#endif /* usci_isr_handler_h */
