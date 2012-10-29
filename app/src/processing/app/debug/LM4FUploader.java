8fde6645d2a2b2dd7c62cc4401ae023e6180bcbd
robertinant <robertinant@yahoo.com>
2012-10-29 14:15:54 -0700

    Change upload tool to lm4flash
    - Added lm4flash.exe to dist files
    - Change build.xml to copy lm4flash into build dir
    - Change LM4FUploader.java to accomodate lm4flash.exe

--- app/src/processing/app/debug/LM4FUploader.java
+++ app/src/processing/app/debug/LM4FUploader.java
@@ -20,17 +20,11 @@ public class LM4FUploader extends Uploader{
 	throws RunnerException, SerialException {
 		this.verbose = verbose;
 		Map<String, String> boardPreferences = Base.getBoardPreferences();
-		//No serial programming support (yet). 
-		//Upload using programmer (LMFlasher for windows and openocd for Mac OS X and Linux).
-		//added support for openocd for windows 
-
 		Target target = Base.getTarget();
 		Collection params = new ArrayList();
 
 		if (Base.isMacOS() || Base.isLinux()) {
-			//params.add(boardPreferences.get("upload.protocol"));
 			if ( Base.isLinux()) {
-        // lm4flash rgb.bin 
 				params.add(buildPath + File.separator + className + ".bin");
 			}
 			else { 
@@ -38,30 +32,8 @@ public class LM4FUploader extends Uploader{
 			}
 			return mspdebug(params);
 		} else {
-			//Standard cmd.exe call: lmflash -q ek-lm4f232 -v -r qs-rgb.bin
-			// code to access via LM4F_Flasher
-			params.add("-q");
-			params.add(boardPreferences.get("build.mcu"));
-			params.add("-v");
-			params.add("-r");
-			//params.add("-w");
 			params.add(buildPath + File.separator + className + ".bin");
-			/*params.add("-g");
-			if(!Preferences.getBoolean("upload.verbose"))
-					params.add("-q");
-			
-			params.add("-z[VCC]");*/
-			return LMFlasher(params);
-			
-//			params.add(boardPreferences.get("upload.protocol"));
-/*			params.add("tilib");       // always use the TI Lib on Windows, best integrate on this OS
-			params.add("-d");//specifies that the driver should connect via a tty device rather than USB
-			params.add("USB");
-			if(!Preferences.getBoolean("upload.verbose"))
-				params.add("-q"); //quiet mode
-			params.add("--force-reset");
-			params.add("\"prog " + buildPath + File.separator + className + ".hex\"");
-			return mspdebug(params);*/
+			return mspdebug(params);
 		}
 	}
 
@@ -74,23 +46,14 @@ public class LM4FUploader extends Uploader{
 		List commandDownloader = new ArrayList();
 
 		if ( Base.isLinux()) {
-			commandDownloader.add(Base.getLM4FBasePath() + "lm4flash"); // "/tools/lm4f/bin/" + "lm4flash"
+			commandDownloader.add(Base.getLM4FBasePath() + "lm4flash");
 		} 
 		else if (Base.isMacOS()) {
-			commandDownloader.add(Base.getHardwarePath() + "/tools/lm4f/openocd/openocd");
+			commandDownloader.add(Base.getLM4FBasePath() + "lm4flash");
 		}
 		else {
-			commandDownloader.add(Base.getHardwarePath() + "\\tools\\lm4f\\openocd\\openocd");
+			commandDownloader.add(Base.getLM4FBasePath() + "lm4flash");
 		}
-		commandDownloader.addAll(params);
-		
-		return executeUploadCommand(commandDownloader);
-	}
-	
-	public boolean LMFlasher(Collection params) throws RunnerException {
-		List commandDownloader = new ArrayList();
-
-		commandDownloader.add(Base.toShortPath(Base.getHardwarePath()) + "\\tools\\lm4f\\LMFlasher\\" + "LMFlash.exe");
 		commandDownloader.addAll(params);
 		
 		return executeUploadCommand(commandDownloader);
