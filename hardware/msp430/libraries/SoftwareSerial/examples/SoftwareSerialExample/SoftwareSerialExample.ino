1f6d78ef8c84dde700c93e19aef4a40f2296879a
Brian Baker <m000z0rz@gmail.com>
2013-02-09 15:32:36 -0600

    Porting SoftwareSerial library to Energia

---
+++ hardware/msp430/libraries/SoftwareSerial/examples/SoftwareSerialExample/SoftwareSerialExample.ino
@@ -0,0 +1,48 @@
+/*
+  Software serial multple serial test
+ 
+ Receives from the hardware serial, sends to software serial.
+ Receives from software serial, sends to hardware serial.
+ 
+ The circuit: 
+ * RX is digital pin P1.4 (connect to TX of other device)
+ * TX is digital pin P1.3 (connect to RX of other device)
+ 
+ created back in the mists of time
+ modified 25 May 2012
+ by Tom Igoe
+ based on Mikal Hart's example
+ modified again 8 Feb 2013
+ by Brian Baker for MSP430
+ 
+ This example code is in the public domain.
+ 
+ */
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(P1_4, P1_3); // RX, TX
+
+void setup()  
+{
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+
+
+  Serial.println("Goodnight moon!");
+
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(9600);
+  mySerial.println("Hello, world?");
+}
+
+void loop() // run over and over
+{
+  if (mySerial.available())
+    Serial.write(mySerial.read());
+  if (Serial.available())
+    mySerial.write(Serial.read());
+}
+
