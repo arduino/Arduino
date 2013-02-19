b8a203714d70cc19f43d6fe60506d33322e54ea7
Angus Gratton <gus@projectgus.com>
2013-02-20 00:17:46 +1100

    Update and test Leonardo port mappings

--- hardware/arduino/variants/leonardo/pins_arduino.h
+++ hardware/arduino/variants/leonardo/pins_arduino.h
@@ -132,8 +132,6 @@ static const uint8_t A11 = 29;	// D12
 extern const uint8_t PROGMEM analog_pin_to_channel_PGM[];
 #define analogPinToChannel(P)  ( pgm_read_byte( analog_pin_to_channel_PGM + (P) ) )
 
-#ifdef ARDUINO_MAIN
-
 // On the Arduino board, digital pins are also used
 // for the analog output (software PWM).  Analog input
 // pins are a separate set.
@@ -177,146 +175,142 @@ extern const uint8_t PROGMEM analog_pin_to_channel_PGM[];
 // these arrays map port names (e.g. port B) to the
 // appropriate addresses for various functions (e.g. reading
 // and writing)
-const uint16_t PROGMEM port_to_mode_PGM[] = {
-	NOT_A_PORT,
-	NOT_A_PORT,
-	(uint16_t) &DDRB,
-	(uint16_t) &DDRC,
-	(uint16_t) &DDRD,
-	(uint16_t) &DDRE,
-	(uint16_t) &DDRF,
-};
+#define PORT_TO_MODE                            \
+  NOT_A_PORT,                                   \
+    NOT_A_PORT,                                 \
+    (uint16_t) &DDRB,                           \
+    (uint16_t) &DDRC,                           \
+    (uint16_t) &DDRD,                           \
+    (uint16_t) &DDRE,                           \
+    (uint16_t) &DDRF,                           
 
-const uint16_t PROGMEM port_to_output_PGM[] = {
-	NOT_A_PORT,
-	NOT_A_PORT,
-	(uint16_t) &PORTB,
-	(uint16_t) &PORTC,
-	(uint16_t) &PORTD,
-	(uint16_t) &PORTE,
-	(uint16_t) &PORTF,
-};
+#define PORT_TO_OUTPUT                          \
+  NOT_A_PORT,                                   \
+    NOT_A_PORT,                                 \
+    (uint16_t) &PORTB,                          \
+    (uint16_t) &PORTC,                          \
+    (uint16_t) &PORTD,                          \
+    (uint16_t) &PORTE,                          \
+    (uint16_t) &PORTF,
 
-const uint16_t PROGMEM port_to_input_PGM[] = {
-	NOT_A_PORT,
-	NOT_A_PORT,
-	(uint16_t) &PINB,
-	(uint16_t) &PINC,
-	(uint16_t) &PIND,
-	(uint16_t) &PINE,
-	(uint16_t) &PINF,
-};
+#define PORT_TO_INPUT                           \
+  NOT_A_PORT,                                   \
+    NOT_A_PORT,                                 \
+    (uint16_t) &PINB,                           \
+    (uint16_t) &PINC,                           \
+    (uint16_t) &PIND,                           \
+    (uint16_t) &PINE,                           \
+    (uint16_t) &PINF,                           \
 
-const uint8_t PROGMEM digital_pin_to_port_PGM[] = {
-	PD, // D0 - PD2
-	PD,	// D1 - PD3
-	PD, // D2 - PD1
-	PD,	// D3 - PD0
-	PD,	// D4 - PD4
-	PC, // D5 - PC6
-	PD, // D6 - PD7
-	PE, // D7 - PE6
-	
-	PB, // D8 - PB4
-	PB,	// D9 - PB5
-	PB, // D10 - PB6
-	PB,	// D11 - PB7
-	PD, // D12 - PD6
-	PC, // D13 - PC7
-	
-	PB,	// D14 - MISO - PB3
-	PB,	// D15 - SCK - PB1
-	PB,	// D16 - MOSI - PB2
-	PB,	// D17 - SS - PB0
-	
-	PF,	// D18 - A0 - PF7
-	PF, // D19 - A1 - PF6
-	PF, // D20 - A2 - PF5
-	PF, // D21 - A3 - PF4
-	PF, // D22 - A4 - PF1
-	PF, // D23 - A5 - PF0
-	
-	PD, // D24 / D4 - A6 - PD4
-	PD, // D25 / D6 - A7 - PD7
-	PB, // D26 / D8 - A8 - PB4
-	PB, // D27 / D9 - A9 - PB5
-	PB, // D28 / D10 - A10 - PB6
-	PD, // D29 / D12 - A11 - PD6
-};
+#define DIGITAL_PIN_TO_PORT                     \
+  PD, /* D0 - PD2 */                            \
+    PD,	/* D1 - PD3 */                          \
+    PD, /* D2 - PD1 */                          \
+    PD,	/* D3 - PD0 */                          \
+    PD,	/* D4 - PD4 */                          \
+    PC, /* D5 - PC6 */                          \
+    PD, /* D6 - PD7 */                          \
+    PE, /* D7 - PE6 */                          \
+                                                \
+    PB, /* D8 - PB4 */                          \
+    PB,	/* D9 - PB5 */                          \
+    PB, /* D10 - PB6 */                         \
+    PB,	/* D11 - PB7 */                         \
+    PD, /* D12 - PD6 */                         \
+    PC, /* D13 - PC7 */                         \
+                                                \
+    PB,	/* D14 - MISO - PB3 */                  \
+    PB,	/* D15 - SCK - PB1 */                   \
+    PB,	/* D16 - MOSI - PB2 */                  \
+    PB,	/* D17 - SS - PB0 */                    \
+                                                \
+    PF,	/* D18 - A0 - PF7 */                    \
+    PF, /* D19 - A1 - PF6 */                    \
+    PF, /* D20 - A2 - PF5 */                    \
+    PF, /* D21 - A3 - PF4 */                    \
+    PF, /* D22 - A4 - PF1 */                    \
+    PF, /* D23 - A5 - PF0 */                    \
+                                                \
+    PD, /* D24 / D4 - A6 - PD4 */               \
+    PD, /* D25 / D6 - A7 - PD7 */               \
+    PB, /* D26 / D8 - A8 - PB4 */               \
+    PB, /* D27 / D9 - A9 - PB5 */               \
+    PB, /* D28 / D10 - A10 - PB6 */             \
+    PD, /* D29 / D12 - A11 - PD6 */               
 
-const uint8_t PROGMEM digital_pin_to_bit_mask_PGM[] = {
-	_BV(2), // D0 - PD2
-	_BV(3),	// D1 - PD3
-	_BV(1), // D2 - PD1
-	_BV(0),	// D3 - PD0
-	_BV(4),	// D4 - PD4
-	_BV(6), // D5 - PC6
-	_BV(7), // D6 - PD7
-	_BV(6), // D7 - PE6
-	
-	_BV(4), // D8 - PB4
-	_BV(5),	// D9 - PB5
-	_BV(6), // D10 - PB6
-	_BV(7),	// D11 - PB7
-	_BV(6), // D12 - PD6
-	_BV(7), // D13 - PC7
-	
-	_BV(3),	// D14 - MISO - PB3
-	_BV(1),	// D15 - SCK - PB1
-	_BV(2),	// D16 - MOSI - PB2
-	_BV(0),	// D17 - SS - PB0
-	
-	_BV(7),	// D18 - A0 - PF7
-	_BV(6), // D19 - A1 - PF6
-	_BV(5), // D20 - A2 - PF5
-	_BV(4), // D21 - A3 - PF4
-	_BV(1), // D22 - A4 - PF1
-	_BV(0), // D23 - A5 - PF0
-	
-	_BV(4), // D24 / D4 - A6 - PD4
-	_BV(7), // D25 / D6 - A7 - PD7
-	_BV(4), // D26 / D8 - A8 - PB4
-	_BV(5), // D27 / D9 - A9 - PB5
-	_BV(6), // D28 / D10 - A10 - PB6
-	_BV(6), // D29 / D12 - A11 - PD6
-};
+#define DIGITAL_PIN_TO_BIT_MASK                 \
+  _BV(2), /* D0 - PD2 */                        \
+    _BV(3),	/* D1 - PD3 */                  \
+    _BV(1), /* D2 - PD1 */                      \
+    _BV(0),	/* D3 - PD0 */                  \
+    _BV(4),	/* D4 - PD4 */                  \
+    _BV(6), /* D5 - PC6 */                      \
+    _BV(7), /* D6 - PD7 */                      \
+    _BV(6), /* D7 - PE6 */                      \
+                                                \
+    _BV(4), /* D8 - PB4 */                      \
+    _BV(5),	/* D9 - PB5 */                  \
+    _BV(6), /* D10 - PB6 */                     \
+    _BV(7),	/* D11 - PB7 */                 \
+    _BV(6), /* D12 - PD6 */                     \
+    _BV(7), /* D13 - PC7 */                     \
+                                                \
+    _BV(3),	/* D14 - MISO - PB3 */          \
+    _BV(1),	/* D15 - SCK - PB1 */           \
+    _BV(2),	/* D16 - MOSI - PB2 */          \
+    _BV(0),	/* D17 - SS - PB0 */            \
+                                                \
+    _BV(7),	/* D18 - A0 - PF7 */            \
+    _BV(6), /* D19 - A1 - PF6 */                \
+    _BV(5), /* D20 - A2 - PF5 */                \
+    _BV(4), /* D21 - A3 - PF4 */                \
+    _BV(1), /* D22 - A4 - PF1 */                \
+    _BV(0), /* D23 - A5 - PF0 */                \
+                                                \
+    _BV(4), /* D24 / D4 - A6 - PD4 */           \
+    _BV(7), /* D25 / D6 - A7 - PD7 */           \
+    _BV(4), /* D26 / D8 - A8 - PB4 */           \
+    _BV(5), /* D27 / D9 - A9 - PB5 */           \
+    _BV(6), /* D28 / D10 - A10 - PB6 */         \
+    _BV(6), /* D29 / D12 - A11 - PD6 */
 
-const uint8_t PROGMEM digital_pin_to_timer_PGM[] = {
-	NOT_ON_TIMER,	
-	NOT_ON_TIMER,
-	NOT_ON_TIMER,
-	TIMER0B,		/* 3 */
-	NOT_ON_TIMER,
-	TIMER3A,		/* 5 */
-	TIMER4D,		/* 6 */
-	NOT_ON_TIMER,	
-	
-	NOT_ON_TIMER,	
-	TIMER1A,		/* 9 */
-	TIMER1B,		/* 10 */
-	TIMER0A,		/* 11 */
-	
-	NOT_ON_TIMER,	
-	TIMER4A,		/* 13 */
-	
-	NOT_ON_TIMER,	
-	NOT_ON_TIMER,
-	NOT_ON_TIMER,
-	NOT_ON_TIMER,
-	NOT_ON_TIMER,
-	NOT_ON_TIMER,
+#define DIGITAL_PIN_TO_TIMER                            \
+  NOT_ON_TIMER,                                         \
+    NOT_ON_TIMER,                                       \
+    NOT_ON_TIMER,                                       \
+    TIMER0B,		/* 3 */                         \
+    NOT_ON_TIMER,                                       \
+    TIMER3A,		/* 5 */                         \
+    TIMER4D,		/* 6 */                         \
+    NOT_ON_TIMER,                                       \
+                                                        \
+    NOT_ON_TIMER,                                       \
+    TIMER1A,		/* 9 */                         \
+    TIMER1B,		/* 10 */                        \
+    TIMER0A,		/* 11 */                        \
+                                                        \
+    NOT_ON_TIMER,                                       \
+    TIMER4A,		/* 13 */                        \
+                                                        \
+    NOT_ON_TIMER,                                       \
+    NOT_ON_TIMER,                                       \
+    NOT_ON_TIMER,                                       \
+    NOT_ON_TIMER,                                       \
+    NOT_ON_TIMER,                                       \
+    NOT_ON_TIMER,                                       \
+                                                        \
+    NOT_ON_TIMER,                                       \
+    NOT_ON_TIMER,                                       \
+    NOT_ON_TIMER,                                       \
+    NOT_ON_TIMER,                                       \
+    NOT_ON_TIMER,                                       \
+    NOT_ON_TIMER,                                       \
+    NOT_ON_TIMER,                                       \
+    NOT_ON_TIMER,                                       \
+    NOT_ON_TIMER,                                       \
+    NOT_ON_TIMER,
 
-	NOT_ON_TIMER,
-	NOT_ON_TIMER,
-	NOT_ON_TIMER,
-	NOT_ON_TIMER,
-	NOT_ON_TIMER,
-	NOT_ON_TIMER,
-	NOT_ON_TIMER,
-	NOT_ON_TIMER,
-	NOT_ON_TIMER,
-	NOT_ON_TIMER,
-};
+#ifdef ARDUINO_MAIN
 
 const uint8_t PROGMEM analog_pin_to_channel_PGM[] = {
 	7,	// A0				PF7					ADC7
@@ -333,5 +327,6 @@ const uint8_t PROGMEM analog_pin_to_channel_PGM[] = {
 	9	// A11		D12		PD6					ADC9
 };
 
-#endif /* ARDUINO_MAIN */
+#endif
+
 #endif /* Pins_Arduino_h */
