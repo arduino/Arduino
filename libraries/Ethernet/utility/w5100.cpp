2ad7193c75ff83acd15c492592a70a885b7989ab
Luke Hovington <lukeh@hovo.id.au>
2013-03-26 02:12:47 +1000

    Added missing variables
    Due to my hardware, I needed to change the chip select pin in the
    ethernet controller, this now gives us the ability to choose the pin on
    setup.
    I have also added getting of the macAddress..

--- libraries/Ethernet/utility/w5100.cpp
+++ libraries/Ethernet/utility/w5100.cpp
@@ -16,6 +16,10 @@
 // W5100 controller instance
 W5100Class W5100;
 
+// Chip select pins
+volatile uint8_t *cs_reg, *cs_out;
+uint8_t  cs_bit;
+
 #define TX_RX_MAX_BUF_SIZE 2048
 #define TX_BUF 0x1100
 #define RX_BUF (TX_BUF + TX_RX_MAX_BUF_SIZE)
