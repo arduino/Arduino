be35ac989f3aef655850fc4fba06095305bfa047
Loren M. Lang <lorenl@north-winds.org>
2013-04-07 00:15:47 -0700

    Cleaned up warnings and fixed array overrun
    Cleaned up warnings about unsigned types, unused variables, and
    declarations.
    
    Explicitly declare char as signed for cases when compiling with
    -funsigned-char.
    
    Fixed strict-aliasing issues in IPAddress.h.
    
    Fixed array overrun where EXTERNAL_NUM_INTERRUPTS was defined too small.

--- hardware/arduino/avr/cores/arduino/IPAddress.h
+++ hardware/arduino/avr/cores/arduino/IPAddress.h
@@ -48,8 +48,14 @@ public:
 
     // Overloaded cast operator to allow IPAddress objects to be used where a pointer
     // to a four-byte uint8_t array is expected
-    operator uint32_t() { return *((uint32_t*)_address); };
-    bool operator==(const IPAddress& addr) { return (*((uint32_t*)_address)) == (*((uint32_t*)addr._address)); };
+    operator uint32_t() { return ((uint32_t)_address[0] << 0 |
+		    		  (uint32_t)_address[1] << 8 |
+				  (uint32_t)_address[2] << 16 |
+				  (uint32_t)_address[3] << 24); }
+    bool operator==(const IPAddress& addr) { return (_address[0] == addr._address[0] &&
+		    				     _address[1] == addr._address[1] &&
+		    				     _address[2] == addr._address[2] &&
+		    				     _address[3] == addr._address[3]); }
     bool operator==(const uint8_t* addr);
 
     // Overloaded index operator to allow getting and setting individual octets of the address
