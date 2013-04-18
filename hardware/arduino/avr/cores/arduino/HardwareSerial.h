b99521de0c3bcc9f1be96a8fb955a8d84ff89ad5
Matthijs Kooijman <matthijs@stdin.nl>
2013-04-18 14:17:47 +0200

    Use constants for register bit positions in HardwareSerial
    Previously, the constants to use for the bit positions of the various
    UARTs were passed to the HardwareSerial constructor. However, this
    meant that whenever these values were used, the had to be indirectly
    loaded, resulting in extra code overhead. Additionally, since there is
    no instruction to shift a value by a variable amount, the 1 << x
    expressions (inside _BV and sbi() / cbi()) would be compiled as a loop
    instead of being evaluated at compiletime.
    
    Now, the HardwareSerial class always uses the constants for the bit
    positions of UART 0 (and some code is present to make sure these
    constants exist, even for targets that only have a single unnumbered
    UART or start at UART1).
    
    This was already done for the TXC0 constant, for some reason. For the
    actual register addresses, this approach does not work, since these are
    of course different between the different UARTs on a single chip.
    
    Of course, always using the UART 0 constants is only correct when the
    constants are actually identical for the different UARTs. It has been
    verified that this is currently the case for all targets supported by
    avr-gcc 4.7.2, and the code contains compile-time checks to verify this
    for the current target, in case a new target is added for which this
    does not hold. This verification was done using:
    
    for i in TXC RXEN TXEN RXCIE UDRIE U2X UPE; do echo $i; grep --no-filename -r "#define $i[0-9]\? " /usr/lib/avr/include/avr/io* | sed "s/#define $i[0-9]\?\s*\(\S\)\+\s*\(\/\*.*\*\/\)\?$/\1/" | sort | uniq ; done
    
    This command shows that the above constants are identical for all uarts
    on all platforms, except for TXC, which is sometimes 6 and sometimes 0.
    Further investigation shows that it is always 6, except in io90scr100.h,
    but that file defines TXC0 with value 6 for the UART and uses TXC with
    value 0 for some USB-related register.
    
    This commit reduces program size on the uno by around 120 bytes.

--- hardware/arduino/avr/cores/arduino/HardwareSerial.h
+++ hardware/arduino/avr/cores/arduino/HardwareSerial.h
@@ -46,11 +46,6 @@ class HardwareSerial : public Stream
     volatile uint8_t *_ucsrb;
     volatile uint8_t *_ucsrc;
     volatile uint8_t *_udr;
-    uint8_t _rxen;
-    uint8_t _txen;
-    uint8_t _rxcie;
-    uint8_t _udrie;
-    uint8_t _u2x;
     bool transmitting;
 
   public:
@@ -68,8 +63,7 @@ class HardwareSerial : public Stream
     HardwareSerial(
       volatile uint8_t *ubrrh, volatile uint8_t *ubrrl,
       volatile uint8_t *ucsra, volatile uint8_t *ucsrb,
-      volatile uint8_t *ucsrc, volatile uint8_t *udr,
-      uint8_t rxen, uint8_t txen, uint8_t rxcie, uint8_t udrie, uint8_t u2x);
+      volatile uint8_t *ucsrc, volatile uint8_t *udr);
     void begin(unsigned long);
     void begin(unsigned long, uint8_t);
     void end();
