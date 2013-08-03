e5805926d0f02c26522de7c8c9c4e21ae589bd80
Alasdair Allan <alasdair@babilim.co.uk>
2013-08-03 21:01:15 +0100

    Added Multicast UDP support
    Fix #1531

--- libraries/Ethernet/src/EthernetUdp.h
+++ libraries/Ethernet/src/EthernetUdp.h
@@ -53,6 +53,7 @@ private:
 public:
   EthernetUDP();  // Constructor
   virtual uint8_t begin(uint16_t);	// initialize, start listening on specified port. Returns 1 if successful, 0 if there are no sockets available to use
+  virtual uint8_t beginMulticast(IPAddress, uint16_t);	// initialize, start listening on specified port. Returns 1 if successful, 0 if there are no sockets available to use
   virtual void stop();  // Finish with the UDP socket
 
   // Sending UDP packets
