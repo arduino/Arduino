a0a50ff17a195e321598fb3b0c4e82204210b6c5
Tayler Mulligan <mulligantayler@gmail.com>
2013-08-27 13:21:39 -0700

    refactored calls to digitalWrite
    additional whitespace was also removed

--- hardware/arduino/cores/arduino/wiring_shift.c
+++ hardware/arduino/cores/arduino/wiring_shift.c
@@ -29,13 +29,12 @@ uint8_t shiftIn(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder) {
 	uint8_t i;
 
 	for (i = 0; i < 8; ++i) {
-		digitalWrite(clockPin, LOW);
 		if (bitOrder == LSBFIRST)
 			value |= digitalRead(dataPin) << i;
 		else
 			value |= digitalRead(dataPin) << (7 - i);
 		digitalWrite(clockPin, HIGH);
-
+		digitalWrite(clockPin, LOW);
 	}
 	return value;
 }
