fa6aa5a24b013ef9c2e7c901fee25957d1fa9fe5
Robert Wessels <robertinant@yahoo.com>
2013-08-31 14:33:38 -0700

    Fix compilation error

--- hardware/msp430/libraries/SoftwareSerial/examples/SoftwareSerialExample/SoftwareSerialExample.ino
+++ hardware/msp430/libraries/SoftwareSerial/examples/SoftwareSerialExample/SoftwareSerialExample.ino
@@ -26,10 +26,6 @@ void setup()
 {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
-
 
   Serial.println("Goodnight moon!");
 
