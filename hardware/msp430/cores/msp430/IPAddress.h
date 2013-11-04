b4b9c0c91f2698804075f15cd9a3ad84fe35420c
Robert Wessels <robertinant@yahoo.com>
2013-11-04 11:52:28 -0800

    Fix (unit32_t) cast operator in IPAddress.h

--- hardware/msp430/cores/msp430/IPAddress.h
+++ hardware/msp430/cores/msp430/IPAddress.h
@@ -48,8 +48,8 @@ public:
 
     // Overloaded cast operator to allow IPAddress objects to be used where a pointer
     // to a four-byte uint8_t array is expected
-    operator uint32_t() { return *(/*(uint32_t*)*/_address); };
-    bool operator==(const IPAddress& addr) { return (*(/*(uint32_t*)*/_address)) == (*(/*(uint32_t*)*/addr._address)); };
+    operator uint32_t() { return *((uint32_t*)_address); };
+    bool operator==(const IPAddress& addr) { return (*((uint32_t*)_address)) == (*((uint32_t*)addr._address)); };
     bool operator==(const uint8_t* addr);
 
     // Overloaded index operator to allow getting and setting individual octets of the address
