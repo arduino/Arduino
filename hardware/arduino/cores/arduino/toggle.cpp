515a20f1d364709c13abe6ab7ea102dc1822aed2
ionut97 <ionut_dragut97@yahoo.com>
2014-01-11 11:24:22 -0800

    Added include guard in toggle.cpp

--- hardware/arduino/cores/arduino/toggle.cpp
+++ hardware/arduino/cores/arduino/toggle.cpp
@@ -1,5 +1,11 @@
+#ifndef TOGGLE_CPP
+#define TOGGLE_CPP
+
+#include "Arduino.h"
 
 inline void toggle (bool &buttonState, bool change)
 {
   buttonState = change ? !buttonState : buttonState;
 }
+
+#endif
