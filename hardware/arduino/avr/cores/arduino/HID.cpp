092dbd7a53bf96eca9209188143eacb1a80e12a5
Jesse Vincent <jesse@bestpractical.com>
2014-02-02 15:18:01 -0500

    clean up and reorder the USB HID descriptors. No functional changes

--- hardware/arduino/avr/cores/arduino/HID.cpp
+++ hardware/arduino/avr/cores/arduino/HID.cpp
@@ -45,23 +45,54 @@ Keyboard_ Keyboard;
 #define RAWHID_TX_SIZE 64
 #define RAWHID_RX_SIZE 64
 
-#define HID_REPORTID_MOUSE (1)
-#define HID_REPORTID_KEYBOARD (2)
-#define HID_REPORTID_RAWHID (3)
+#define HID_REPORTID_KEYBOARD (1)
+#define HID_REPORTID_MOUSE (2)
+#define HID_REPORTID_MOUSE_ABS (3)
 #define HID_REPORTID_SYSTEMCONTROL (4)
-#define HID_REPORTID_MOUSE_ABS (5)
-#define HID_REPORTID_CONSUMERCONTROL (6)
+#define HID_REPORTID_CONSUMERCONTROL (5)
+#define HID_REPORTID_RAWHID (6)
 
 extern const u8 _hidReportDescriptor[] PROGMEM;
 const u8 _hidReportDescriptor[] = {
 	
+    //  Keyboard
+    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)	// 47
+    0x09, 0x06,                    // USAGE (Keyboard)
+    0xa1, 0x01,                    // COLLECTION (Application)
+    0x85, HID_REPORTID_KEYBOARD,   //   REPORT_ID
+    0x05, 0x07,                    //   USAGE_PAGE (Keyboard)
+
+    // Keyboard Modifiers (shift, alt, ...)
+    0x19, 0xe0,                    //   USAGE_MINIMUM (Keyboard LeftControl)
+    0x29, 0xe7,                    //   USAGE_MAXIMUM (Keyboard Right GUI)
+    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+    0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
+    0x75, 0x01,                    //   REPORT_SIZE (1)
+
+    0x95, 0x08,                    //   REPORT_COUNT (8)
+    0x81, 0x02,                    //   INPUT (Data,Var,Abs)
+    0x95, 0x01,                    //   REPORT_COUNT (1)
+    0x75, 0x08,                    //   REPORT_SIZE (8)
+    0x81, 0x03,                    //   INPUT (Cnst,Var,Abs)
+
+    // Keyboard keys
+    0x95, 0x06,                    //   REPORT_COUNT (6)
+    0x75, 0x08,                    //   REPORT_SIZE (8)
+    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+    0x26, 0xDF, 0x00,              //   LOGICAL_MAXIMUM (239)
+    0x05, 0x07,                    //   USAGE_PAGE (Keyboard)
+    0x19, 0x00,                    //   USAGE_MINIMUM (Reserved (no event indicated))
+    0x29, 0xDF,                    //   USAGE_MAXIMUM (Left Control - 1)
+    0x81, 0x00,                    //   INPUT (Data,Ary,Abs)
+    0xc0,                          // END_COLLECTION
+
     //  Mouse relative
     0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)	// 54
     0x09, 0x02,                    // USAGE (Mouse)
     0xa1, 0x01,                    // COLLECTION (Application)
     0x09, 0x01,                    //   USAGE (Pointer)
     0xa1, 0x00,                    //   COLLECTION (Physical)
-    0x85, HID_REPORTID_MOUSE,      //     REPORT_ID (1)
+    0x85, HID_REPORTID_MOUSE,      //     REPORT_ID
     0x05, 0x09,                    //     USAGE_PAGE (Button)
     0x19, 0x01,                    //     USAGE_MINIMUM (Button 1)
     0x29, 0x03,                    //     USAGE_MAXIMUM (Button 3)
@@ -92,7 +123,7 @@ const u8 _hidReportDescriptor[] = {
     0xa1, 0x01,                    // COLLECTION (Application)
     0x09, 0x01,                    //   USAGE (Pointer)
     0xa1, 0x00,                    //   COLLECTION (Physical)
-    0x85, HID_REPORTID_MOUSE_ABS,  //     REPORT_ID (5)
+    0x85, HID_REPORTID_MOUSE_ABS,  //     REPORT_ID
     0x05, 0x09,                    //     USAGE_PAGE (Button)
     0x19, 0x01,                    //     USAGE_MINIMUM (Button 1)
     0x29, 0x03,                    //     USAGE_MAXIMUM (Button 3)
@@ -116,106 +147,76 @@ const u8 _hidReportDescriptor[] = {
     0xc0,                          // END_COLLECTION
 #endif
 
-    //  Keyboard
-    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)	// 47
-    0x09, 0x06,                    // USAGE (Keyboard)
-    0xa1, 0x01,                    // COLLECTION (Application)
-    0x85, HID_REPORTID_KEYBOARD,   //   REPORT_ID (2)
-    0x05, 0x07,                    //   USAGE_PAGE (Keyboard)
-   
-    // Keyboard Modifiers (shift, alt, ...)
-    0x19, 0xe0,                    //   USAGE_MINIMUM (Keyboard LeftControl)
-    0x29, 0xe7,                    //   USAGE_MAXIMUM (Keyboard Right GUI)
-    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-    0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
-    0x75, 0x01,                    //   REPORT_SIZE (1)
-    
-	0x95, 0x08,                    //   REPORT_COUNT (8)
-    0x81, 0x02,                    //   INPUT (Data,Var,Abs)
-    0x95, 0x01,                    //   REPORT_COUNT (1)
-    0x75, 0x08,                    //   REPORT_SIZE (8)
-    0x81, 0x03,                    //   INPUT (Cnst,Var,Abs)
-
-    // Keyboard keys
-    0x95, 0x06,                    //   REPORT_COUNT (6)
-    0x75, 0x08,                    //   REPORT_SIZE (8)
-    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-    0x26, 0xDF, 0x00,              //   LOGICAL_MAXIMUM (239)
-    0x05, 0x07,                    //   USAGE_PAGE (Keyboard)
-    0x19, 0x00,                    //   USAGE_MINIMUM (Reserved (no event indicated))
-    0x29, 0xDF,                    //   USAGE_MAXIMUM (Left Control - 1)
-    0x81, 0x00,                    //   INPUT (Data,Ary,Abs)
-    0xc0,                          // END_COLLECTION
-
     //  System Control (Power Down, Sleep, Wakeup, ...)
-    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
-    0x09, 0x80,                    // USAGE (System Control)
-    0xa1, 0x01,                    // COLLECTION (Application)
-    0x85, HID_REPORTID_SYSTEMCONTROL,// REPORT_ID (4)
-    0x09, 0x81,                    //   USAGE (System Power Down)
-    0x09, 0x82,                    //   USAGE (System Sleep)
-    0x09, 0x83,                    //   USAGE (System Wakeup)
-    0x09, 0x8E,                    //   USAGE (System Cold Restart)
-    0x09, 0x8F,                    //   USAGE (System Warm Restart)
-    0x09, 0xA0,                    //   USAGE (System Dock)
-    0x09, 0xA1,                    //   USAGE (System Undock)
-    0x09, 0xA7,                    //   USAGE (System Speaker Mute)
-    0x09, 0xA8,                    //   USAGE (System Hibernate)
-    // although these display usages are not that important, they don't cost much more than declaring
-    // the otherwise necessary constant fill bits
-    0x09, 0xB0,                    //   USAGE (System Display Invert)
-    0x09, 0xB1,                    //   USAGE (System Display Internal)
-    0x09, 0xB2,                    //   USAGE (System Display External)
-    0x09, 0xB3,                    //   USAGE (System Display Both)
-    0x09, 0xB4,                    //   USAGE (System Display Dual)
-    0x09, 0xB5,                    //   USAGE (System Display Toggle Intern/Extern)
-    0x09, 0xB6,                    //   USAGE (System Display Swap)
-    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-    0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
-    0x75, 0x01,                    //   REPORT_SIZE (1)
-    0x95, 0x10,                    //   REPORT_COUNT (16)
-    0x81, 0x02,                    //   INPUT (Data,Var,Abs)
-    0xc0,                          // END_COLLECTION
+    0x05, 0x01,                        // USAGE_PAGE (Generic Desktop)
+    0x09, 0x80,                        // USAGE (System Control)
+    0xa1, 0x01,                        // COLLECTION (Application)
+    0x85, HID_REPORTID_SYSTEMCONTROL,  // REPORT_ID
+    0x09, 0x81,                        //   USAGE (System Power Down)
+    0x09, 0x82,                        //   USAGE (System Sleep)
+    0x09, 0x83,                        //   USAGE (System Wakeup)
+    0x09, 0x8E,                        //   USAGE (System Cold Restart)
+    0x09, 0x8F,                        //   USAGE (System Warm Restart)
+    0x09, 0xA0,                        //   USAGE (System Dock)
+    0x09, 0xA1,                        //   USAGE (System Undock)
+    0x09, 0xA7,                        //   USAGE (System Speaker Mute)
+    0x09, 0xA8,                        //   USAGE (System Hibernate)
+    // although these display usages are not that important, they don't cost
+    // much more than declaring the otherwise necessary constant fill bits
+    0x09, 0xB0,                        //   USAGE (System Display Invert)
+    0x09, 0xB1,                        //   USAGE (System Display Internal)
+    0x09, 0xB2,                        //   USAGE (System Display External)
+    0x09, 0xB3,                        //   USAGE (System Display Both)
+    0x09, 0xB4,                        //   USAGE (System Display Dual)
+    0x09, 0xB5,                        //   USAGE (System Display Toggle Intern/Extern)
+    0x09, 0xB6,                        //   USAGE (System Display Swap)
+    0x15, 0x00,                        //   LOGICAL_MINIMUM (0)
+    0x25, 0x01,                        //   LOGICAL_MAXIMUM (1)
+    0x75, 0x01,                        //   REPORT_SIZE (1)
+    0x95, 0x10,                        //   REPORT_COUNT (16)
+    0x81, 0x02,                        //   INPUT (Data,Var,Abs)
+    0xc0,                              // END_COLLECTION
+
+    // Consumer Control (Sound/Media keys)
+    0x05, 0x0c,                        // USAGE_PAGE (Consumer Devices) 
+    0x09, 0x01,                        // USAGE (Consumer Control) 
+    0xa1, 0x01,                        // COLLECTION (Application) 
+    0x85, HID_REPORTID_CONSUMERCONTROL,// REPORT_ID
+    0x15, 0x00,                        // LOGICAL_MINIMUM (0) 
+    0x25, 0x01,                        // LOGICAL_MAXIMUM (1) 
+    0x75, 0x01,                        // REPORT_SIZE (1) 
+    0x95, 0x08,                        // REPORT_COUNT (8) 
+    0x09, 0xe2,                        // USAGE (Mute) 0x01 
+    0x09, 0xe9,                        // USAGE (Volume Up) 0x02 
+    0x09, 0xea,                        // USAGE (Volume Down) 0x03 
+    0x09, 0xcd,                        // USAGE (Play/Pause) 0x04 
+    0x09, 0xb7,                        // USAGE (Stop) 0x05 
+    0x09, 0xb6,                        // USAGE (Scan Previous Track) 0x06 
+    0x09, 0xb5,                        // USAGE (Scan Next Track) 0x07 
+    0x09, 0xb8,                        // USAGE (Eject) 0x08
+    0x81, 0x02,                        // INPUT (Data,Var,Abs)
+    0xc0, 
 
 #if RAWHID_ENABLED
-	//	RAW HID
-	0x06, LSB(RAWHID_USAGE_PAGE), MSB(RAWHID_USAGE_PAGE),	// 30
-	0x0A, LSB(RAWHID_USAGE), MSB(RAWHID_USAGE),
+    //    RAW HID
+    0x06, LSB(RAWHID_USAGE_PAGE), MSB(RAWHID_USAGE_PAGE),    // 30
+    0x0A, LSB(RAWHID_USAGE), MSB(RAWHID_USAGE),
 
-	0xA1, 0x01,				// Collection 0x01
-	0x85, HID_REPORTID_RAWHID,		// REPORT_ID (3)
-	0x75, 0x08,				// report size = 8 bits
-	0x15, 0x00,				// logical minimum = 0
-	0x26, 0xFF, 0x00,		// logical maximum = 255
+    0xA1, 0x01,                // Collection 0x01
+    0x85, HID_REPORTID_RAWHID, // REPORT_ID
+    0x75, 0x08,                // report size = 8 bits
+    0x15, 0x00,                // logical minimum = 0
+    0x26, 0xFF, 0x00,          // logical maximum = 255
 
-	0x95, 64,				// report count TX
-	0x09, 0x01,				// usage
-	0x81, 0x02,				// Input (array)
+    0x95, 64,                  // report count TX
+    0x09, 0x01,                // usage
+    0x81, 0x02,                // Input (array)
 
-	0x95, 64,				// report count RX
-	0x09, 0x02,				// usage
-	0x91, 0x02,				// Output (array)
-	0xC0					// end collection
+    0x95, 64,                  // report count RX
+    0x09, 0x02,                // usage
+    0x91, 0x02,                // Output (array)
+    0xC0                       // end collection
 #endif
-        0x05, 0x0c,                     // USAGE_PAGE (Consumer Devices) 
-         0x09, 0x01,                    // USAGE (Consumer Control) 
-         0xa1, 0x01,                    // COLLECTION (Application) 
-         0x85, HID_REPORTID_CONSUMERCONTROL,                    // REPORT_ID (6) 
-         0x15, 0x00,                    // LOGICAL_MINIMUM (0) 
-         0x25, 0x01,                    // LOGICAL_MAXIMUM (1) 
-         0x75, 0x01,                    // REPORT_SIZE (1) 
-         0x95, 0x08,                    // REPORT_COUNT (8) 
-         0x09, 0xe2,                    // USAGE (Mute) 0x01 
-         0x09, 0xe9,                    // USAGE (Volume Up) 0x02 
-         0x09, 0xea,                    // USAGE (Volume Down) 0x03 
-         0x09, 0xcd,                    // USAGE (Play/Pause) 0x04 
-         0x09, 0xb7,                    // USAGE (Stop) 0x05 
-         0x09, 0xb6,                    // USAGE (Scan Previous Track) 0x06 
-         0x09, 0xb5,                    // USAGE (Scan Next Track) 0x07 
-         0x09, 0xb8,                    // USAGE (Eject) 0x08
-         0x81, 0x02,                    //   INPUT (Data,Var,Abs)
-         0xc0, 
-
 };
 
 extern const HIDDescriptor _hidInterface PROGMEM;
