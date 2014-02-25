30a3a4dfc8c53d31044c29f51f22628c074c9a49
Craig Hollabaugh <craig@hollabaugh.com>
2014-02-24 22:37:53 -0700

    working version

--- libraries/Ethernet/examples/UDPSendReceiveString/UDPSendReceiveString.ino
+++ libraries/Ethernet/examples/UDPSendReceiveString/UDPSendReceiveString.ino
@@ -1,34 +1,38 @@
-/*
-  UDPSendReceive.pde:
+/* 
+ UDPSendReceive.ino
+
  This sketch receives UDP message strings, prints them to the serial port
  and sends an "acknowledge" string back to the sender
- 
+
  A Processing sketch is included at the end of file that can be used to send 
- and received messages for testing with a computer.
+ and received messages for testing with a computer. 
+
+ Here's using netcat to send udp
+     echo -n "foo" | nc -4u -q1 192.168.1.177 8888
  
  created 21 Aug 2010
  by Michael Margolis
+
+ modified 24 Feb 2014
+ by Craig Hollabaugh
  
  This code is in the public domain.
- */
+*/
 
 
-#include <SPI.h>         // needed for Arduino versions later than 0018
 #include <Ethernet.h>
 #include <EthernetUdp.h>         // UDP library from: bjoern@cs.stanford.edu 12/30/2008
 
-
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
-byte mac[] = {  
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(192, 168, 1, 177);
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+IPAddress ip(192,168,1,177);
 
 unsigned int localPort = 8888;      // local port to listen on
 
 // buffers for receiving and sending data
 char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
-char  ReplyBuffer[] = "acknowledged";       // a string to send back
+char  ReplyBuffer[] = "acknowledged\n";       // a string to send back
 
 // An EthernetUDP instance to let us send and receive packets over UDP
 EthernetUDP Udp;
@@ -39,22 +43,24 @@ void setup() {
   Udp.begin(localPort);
 
   Serial.begin(9600);
+  Serial.println("UDPSendReceiveString setup");
+  Serial.print("listening on ");
+  ip.printTo(Serial);
+  Serial.print(":");
+  Serial.println(localPort);
 }
 
 void loop() {
   // if there's data available, read a packet
   int packetSize = Udp.parsePacket();
-  if(packetSize)
-  {
+  if(packetSize) {
     Serial.print("Received packet of size ");
     Serial.println(packetSize);
     Serial.print("From ");
     IPAddress remote = Udp.remoteIP();
-    for (int i =0; i < 4; i++)
-    {
+    for (int i =0; i < 4; i++) {
       Serial.print(remote[i], DEC);
-      if (i < 3)
-      {
+      if (i < 3) {
         Serial.print(".");
       }
     }
@@ -87,31 +93,28 @@ void loop() {
  
  UDP udp;  // define the UDP object
  
- 
  void setup() {
- udp = new UDP( this, 6000 );  // create a new datagram connection on port 6000
- //udp.log( true ); 		// <-- printout the connection activity
- udp.listen( true );           // and wait for incoming message  
+   udp = new UDP( this, 6000 );  // create a new datagram connection on port 6000
+   //udp.log( true ); 		// <-- printout the connection activity
+   udp.listen( true );           // and wait for incoming message  
  }
  
- void draw()
- {
+ void draw() {
  }
  
  void keyPressed() {
- String ip       = "192.168.1.177";	// the remote IP address
- int port        = 8888;		// the destination port
- 
- udp.send("Hello World", ip, port );   // the message to send
- 
+   String ip       = "192.168.1.177";	// the remote IP address
+   int port        = 8888;		// the destination port
+   
+   udp.send("Hello World", ip, port );   // the message to send
  }
  
  void receive( byte[] data ) { 			// <-- default handler
- //void receive( byte[] data, String ip, int port ) {	// <-- extended handler
- 
- for(int i=0; i < data.length; i++) 
- print(char(data[i]));  
- println();   
+   //void receive( byte[] data, String ip, int port ) {	// <-- extended handler
+   
+   for(int i=0; i < data.length; i++) 
+   print(char(data[i]));  
+   println();   
  }
  */
 
