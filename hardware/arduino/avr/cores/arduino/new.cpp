375834da467302c6518aea0ba8429f33f4792929
cider101 <cider101@ciderspace.ch>
2014-03-12 16:24:25 +0100

    Removed unneseccary semi-colons (--pedantic)

--- hardware/arduino/avr/cores/arduino/new.cpp
+++ hardware/arduino/avr/cores/arduino/new.cpp
@@ -20,9 +20,9 @@ void operator delete[](void * ptr)
   free(ptr);
 }
 
-int __cxa_guard_acquire(__guard *g) {return !*(char *)(g);};
-void __cxa_guard_release (__guard *g) {*(char *)g = 1;};
-void __cxa_guard_abort (__guard *) {}; 
+int __cxa_guard_acquire(__guard *g) {return !*(char *)(g);}
+void __cxa_guard_release (__guard *g) {*(char *)g = 1;}
+void __cxa_guard_abort (__guard *) {}
 
-void __cxa_pure_virtual(void) {};
+void __cxa_pure_virtual(void) {}
 
