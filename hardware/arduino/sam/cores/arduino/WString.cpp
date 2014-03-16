555800b32dc980467e91512a010ed84a4cc5ef84
Matthijs Kooijman <matthijs@stdin.nl>
2014-03-16 10:37:38 +0100

    Add String(char *, unsigned) constructor
    This constructor allows converting a buffer containing a
    non-nul-terminated string to a String object, by explicitely passing the
    length.

--- hardware/arduino/sam/cores/arduino/WString.cpp
+++ hardware/arduino/sam/cores/arduino/WString.cpp
@@ -33,6 +33,12 @@ String::String(const char *cstr)
 	if (cstr) copy(cstr, strlen(cstr));
 }
 
+String::String(const char *cstr, unsigned int length)
+{
+	init();
+	if (cstr) copy(cstr, length);
+}
+
 String::String(const String &value)
 {
 	init();
--- hardware/arduino/sam/cores/arduino/WString.h
+++ hardware/arduino/sam/cores/arduino/WString.h
@@ -57,6 +57,7 @@ public:
 	// fails, the string will be marked as invalid (i.e. "if (s)" will
 	// be false).
 	String(const char *cstr = "");
+	String(const char *cstr, unsigned int length);
 	String(const String &str);
 	String(const __FlashStringHelper *str);
        #if __cplusplus >= 201103L || defined(__GXX_EXPERIMENTAL_CXX0X__)
