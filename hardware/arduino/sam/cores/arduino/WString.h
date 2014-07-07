ebeb7843d2d9d7369db7c1f7baad71e1909c1d44
Matthijs Kooijman <matthijs@stdin.nl>
2014-07-07 19:54:31 +0200

    Check for __cplusplus >= 201103L as well as __GXX_EXPERIMENTAL_CXX0X__
    Gcc 4.8 defines __cplusplus as 201103L, so we can check for that now. It
    still also defines __GXX_EXPERIMENTAL_CXX0X__, but this could help on
    other compilers, or if gcc ever decides to stop defining the
    experimental macro.

--- hardware/arduino/sam/cores/arduino/WString.h
+++ hardware/arduino/sam/cores/arduino/WString.h
@@ -59,7 +59,7 @@ public:
 	String(const char *cstr = "");
 	String(const String &str);
 	String(const __FlashStringHelper *str);
-	#ifdef __GXX_EXPERIMENTAL_CXX0X__
+       #if __cplusplus >= 201103L || defined(__GXX_EXPERIMENTAL_CXX0X__)
 	String(String &&rval);
 	String(StringSumHelper &&rval);
 	#endif
@@ -86,7 +86,7 @@ public:
 	String & operator = (const String &rhs);
 	String & operator = (const char *cstr);
 	String & operator = (const __FlashStringHelper *str);
-	#ifdef __GXX_EXPERIMENTAL_CXX0X__
+       #if __cplusplus >= 201103L || defined(__GXX_EXPERIMENTAL_CXX0X__)
 	String & operator = (String &&rval);
 	String & operator = (StringSumHelper &&rval);
 	#endif
@@ -200,7 +200,7 @@ protected:
 	// copy and move
 	String & copy(const char *cstr, unsigned int length);
 	String & copy(const __FlashStringHelper *pstr, unsigned int length);
-	#ifdef __GXX_EXPERIMENTAL_CXX0X__
+       #if __cplusplus >= 201103L || defined(__GXX_EXPERIMENTAL_CXX0X__)
 	void move(String &rhs);
 	#endif
 };
