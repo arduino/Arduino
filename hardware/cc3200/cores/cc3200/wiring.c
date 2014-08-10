a26060ae251b883aad0c0cc4402b6648943a4d7d
Robert Wessels <robertinant@yahoo.com>
2014-08-10 14:34:59 -0700

    Add analogRead()

--- hardware/cc3200/cores/cc3200/wiring.c
+++ hardware/cc3200/cores/cc3200/wiring.c
@@ -84,14 +84,10 @@ void registerSysTickCb(void (*userFunc)(uint32_t))
 	}
 }
 
-uint8_t state = 0;
-
 void SysTickIntHandler(void)
 {
 	milliseconds++;
 
-	state = !state;
-//	digitalWrite(RED_LED, state);
 	uint8_t i;
 	for (i=0; i<8; i++) {
 		if (SysTickCbFuncs[i])
--- hardware/cc3200/cores/cc3200/wiring_analog.c
+++ hardware/cc3200/cores/cc3200/wiring_analog.c
@@ -34,7 +34,10 @@
 #include <driverlib/rom_map.h>
 #include <driverlib/pin.h>
 #include <driverlib/timer.h>
+#include <driverlib/adc.h>
 #include <inc/hw_memmap.h>
+#include <inc/hw_gprcm.h>
+#include <inc/hw_adc.h>
 
 #define TIMER_INTERVAL_RELOAD   40035//255*157
 #define DUTYCYCLE_GRANULARITY   157
@@ -63,9 +66,33 @@ void analogWrite(uint8_t pin, int val) {
 	MAP_PRCMPeripheralClkEnable(PRCM_TIMERA0 + (timer/2), PRCM_RUN_MODE_CLK);
 
 	uint16_t pnum = digitalPinToPinNum(pin);
-	MAP_PinTypeTimer(pnum, PIN_MODE_3);
+
+	switch(timer) {
+	/* PWM0/1 */
+	case TIMERA0A:
+	case TIMERA0B:
+		MAP_PinTypeTimer(pnum, PIN_MODE_5);
+		break;
+ 	/* PWM2/3 */
+	case TIMERA1A:
+	case TIMERA1B:
+		MAP_PinTypeTimer(pnum, PIN_MODE_9);
+		break;
+	/* PWM4/5 */
+	case TIMERA2A:
+	case TIMERA2B:
+		MAP_PinTypeTimer(pnum, PIN_MODE_3);
+		break;
+	/* PWM6/7 */
+	case TIMERA3A:
+	case TIMERA3B:
+		MAP_PinTypeTimer(pnum, PIN_MODE_3);
+		break;
+	}
 
 	uint32_t base = TIMERA0_BASE + ((timer/2) << 12);
+
+	/* FIXME: If B is already opperational and configure A, B get's messed up. */
 	MAP_TimerConfigure(base, TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_PWM | TIMER_CFG_B_PWM);
 
 	uint16_t timerab = timer % 2 ? TIMER_B : TIMER_A;
@@ -78,4 +105,45 @@ void analogWrite(uint8_t pin, int val) {
 	MAP_TimerMatchSet(base, timerab, (val * DUTYCYCLE_GRANULARITY));
 
 	MAP_TimerEnable(base, timerab);
+}
+
+
+uint16_t analogRead(uint8_t pin)
+{
+	uint16_t channel;
+
+	uint16_t pinNum = digitalPinToPinNum(pin);
+
+#if 0
+	/* Enable ADC clocks.###IMPORTANT###Need to be removed for PG 1.32 */
+	HWREG(GPRCM_BASE + GPRCM_O_ADC_CLK_CONFIG) = 0x00000043;
+	HWREG(ADC_BASE + ADC_O_ADC_CTRL) = 0x00000004;
+	HWREG(ADC_BASE + ADC_O_ADC_SPARE0) = 0x00000100;
+	HWREG(ADC_BASE + ADC_O_ADC_SPARE1) = 0x0355AA00;
+#endif
+
+	switch(pinNum) {
+		case PIN_57: {channel = ADC_CH_0;}break;
+		case PIN_58: {channel = ADC_CH_1;}break;
+		case PIN_59: {channel = ADC_CH_2;}break;
+		case PIN_60: {channel = ADC_CH_3;}break;
+		default: return 0;
+	}
+
+	PinTypeADC(pinNum,0xFF);
+	ADCChannelEnable(ADC_BASE, channel);
+	ADCTimerConfig(ADC_BASE,2^17);
+	ADCTimerEnable(ADC_BASE);
+	ADCEnable(ADC_BASE);
+
+	while(!ADCFIFOLvlGet(ADC_BASE, channel));
+
+	uint16_t val = ADCFIFORead(ADC_BASE, channel) & 0x3FFF;
+
+	ADCDisable(ADC_BASE);
+	ADCChannelDisable(ADC_BASE, channel);
+	ADCTimerDisable(ADC_BASE);
+
+	val = val >> 2;
+	return val;
 }
