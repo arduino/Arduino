e47e9e6ee1c2a65341c15a755819c5acacc2f777
Eric Brundick <spirilis@linux.com>
2014-10-16 16:56:37 -0400

    g_StatMem now in FRAM (msp430), INADDR_NONE/MACADDR_NONE now single-allocated and declared extern in .h files.

--- hardware/msp430/libraries/WiFi/utility/driver.c
+++ hardware/msp430/libraries/WiFi/utility/driver.c
@@ -102,6 +102,9 @@ typedef struct
     UINT8 AsyncRespBuf[SL_ASYNC_MAX_MSG_LEN];
 }_SlStatMem_t;
 
+#ifdef __MSP430_HAS_FRAM__
+__attribute__((section(".text")))
+#endif
 _SlStatMem_t g_StatMem;
 #endif
 
