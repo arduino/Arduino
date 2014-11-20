c5591e13f401e4201cce8665475abdceeed228aa
Robert Wessels <robertinant@yahoo.com>
2014-11-20 09:54:28 -0800

    Add sslConnect() to Client class

--- hardware/cc3200/cores/cc3200/Client.h
+++ hardware/cc3200/cores/cc3200/Client.h
@@ -28,6 +28,8 @@ class Client : public Stream {
 public:
   virtual int connect(IPAddress ip, uint16_t port) =0;
   virtual int connect(const char *host, uint16_t port) =0;
+  virtual int sslConnect(const char *host, uint16_t port) = 0;
+  virtual int sslConnect(IPAddress ip, uint16_t port) = 0;
   virtual size_t write(uint8_t) =0;
   virtual size_t write(const uint8_t *buf, size_t size) =0;
   virtual int available() = 0;
