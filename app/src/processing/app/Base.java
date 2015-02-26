6116c9200eaa7ce275981b82b9c4839d9560d18e
Robert Wessels <robertinant@yahoo.com>
2015-02-26 14:40:53 -0800

    Up version to 0101E0015b1

--- app/src/processing/app/Base.java
+++ app/src/processing/app/Base.java
@@ -44,9 +44,9 @@ import static processing.app.I18n._;
  */
 public class Base {
   public static final int REVISION = 101;
-  public static final int EREVISION = 14;
+  public static final int EREVISION = 15;
   /** This might be replaced by main() if there's a lib/version.txt file. */
-  static String VERSION_NAME = "0101E0014";
+  static String VERSION_NAME = "0101E0015b1";
   /** Set true if this a proper release rather than a numbered revision. */
   static public boolean RELEASE = false;
 
