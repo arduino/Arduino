a4d48be7bce7395e95793b6a90ac0b3c073017f9
ricardojlrufino <ricardo.jl.rufino@gmail.com>
2015-03-10 18:58:19 -0300

    remove java7 generic JList<?>

--- arduino-autocomplete/src/cc/arduino/packages/autocomplete/CompletionsRenderer.java
+++ arduino-autocomplete/src/cc/arduino/packages/autocomplete/CompletionsRenderer.java
@@ -67,7 +67,7 @@ public class CompletionsRenderer extends DefaultListCellRenderer {
   }
   
   @Override
-  public Component getListCellRendererComponent(JList<?> list, Object value,
+  public Component getListCellRendererComponent(JList list, Object value,
                                                 int index, boolean isSelected,
                                                 boolean cellHasFocus) {
     
