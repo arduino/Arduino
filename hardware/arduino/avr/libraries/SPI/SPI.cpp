e7c1992b2c5cb2fde5171726baff4b68b8b082f0
Victor Aprea <victor.aprea@wickeddevice.com>
2015-03-17 18:19:36 -0400

    adding back pinMode(SS, OUTPUT) based on testing feedback from @facchinm

--- hardware/arduino/avr/libraries/SPI/SPI.cpp
+++ hardware/arduino/avr/libraries/SPI/SPI.cpp
@@ -42,7 +42,7 @@ void SPIClass::begin()
     // When the SS pin is set as OUTPUT, it can be used as
     // a general purpose output port (it doesn't influence
     // SPI operations).
-    // pinMode(SS, OUTPUT); //an input with internal pullup enabled suffices too
+    pinMode(SS, OUTPUT); //an input with internal pullup enabled suffices too
 
     // Warning: if the SS pin ever becomes a LOW INPUT then SPI
     // automatically switches to Slave, so the data direction of
