071d3f28ab6bf430e950d36e3146af6daa61f948
Robert Wessels <robertinant@yahoo.com>
2015-03-22 12:04:36 -0700

    Add Temboo library to MSP432

---
+++ hardware/msp432/libraries/Temboo/examples/SendDataToGoogleSpreadsheet/TembooAccount.h
@@ -0,0 +1,7 @@
+#define TEMBOO_ACCOUNT "myTembooAccountName"  // your Temboo account name 
+#define TEMBOO_APP_KEY_NAME "myFirstApp"  // your Temboo app name
+#define TEMBOO_APP_KEY  "xxxxxxxxxxxxxx"  // your Temboo app key
+
+#define WIFI_SSID "myWiFiNetworkName"
+#define WIFI_PASSWORD "myWiFiNetworkPassword"
+
