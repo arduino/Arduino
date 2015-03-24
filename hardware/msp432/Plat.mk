47d7948391b1be1a8499444b343757c2aef0efa1
Robert Wessels <robertinant@yahoo.com>
2015-03-24 08:18:24 -0700

    Rearange CPPFLAGS/CFLAGS

--- hardware/msp432/Plat.mk
+++ hardware/msp432/Plat.mk
@@ -1,9 +1,8 @@
-# define CC3200 SDK libs and headers based on definitions above
 SDK_LIBS := #$(APPLICATION_PATH)/hardware/secret/cc3200-sdk/simplelink/gcc/exe/libsimplelink.a
-
 SDK_INCS := -I$(APPLICATION_PATH)/hardware/msp432/cores/msp432/inc/CMSIS
 
 VFP := -mfloat-abi=hard -mfpu=fpv4-sp-d16 -fsingle-precision-constant
 
-CFLAGS := -DF_CPU=$(F_CPU) -D__MSP432P401R__ -DTARGET_IS_MSP432P4XX -Dxdc__nolocalstring=1
+CPPFLAGS +=
+CFLAGS += -DF_CPU=$(F_CPU) -D__MSP432P401R__ -DTARGET_IS_MSP432P4XX -Dxdc__nolocalstring=1
 #SDK_LIBS := ti/runtime/wiring/msp432/lib/ti.runtime.wiring.msp432.lib
