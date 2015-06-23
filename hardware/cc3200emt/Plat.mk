2144459c2bca186fa7face5b296de581d7e6c6c7
Robert Wessels <robertinant@yahoo.com>
2015-06-23 10:54:31 -0700

    Remove cc3200-sdk dependency from Plat.mk for cc3200

--- hardware/cc3200emt/Plat.mk
+++ hardware/cc3200emt/Plat.mk
@@ -1,9 +1,9 @@
 # define CC3200 SDK libs and headers based on definitions above
-SDK_LIBS := $(APPLICATION_PATH)/hardware/cc3200emt/cc3200-sdk/simplelink/gcc/exe/libsimplelink.a \
-           $(APPLICATION_PATH)/hardware/cc3200emt/cc3200-sdk/oslib/gcc/exe/libtirtos.a
+#SDK_LIBS := $(APPLICATION_PATH)/hardware/cc3200emt/cc3200-sdk/simplelink/gcc/exe/libsimplelink.a \
+#           $(APPLICATION_PATH)/hardware/cc3200emt/cc3200-sdk/oslib/gcc/exe/libtirtos.a
 
-SDK_INCS := -I$(APPLICATION_PATH)/hardware/cc3200emt/cc3200-sdk/simplelink/include \
-           -I$(APPLICATION_PATH)/hardware/cc3200emt/cc3200-sdk
+#SDK_INCS := -I$(APPLICATION_PATH)/hardware/cc3200emt/cc3200-sdk/simplelink/include \
+#           -I$(APPLICATION_PATH)/hardware/cc3200emt/cc3200-sdk
 
 #VFP := -mcpu=cortex-m4 -mfloat-abi=soft
 VFP := -mfloat-abi=soft
