ea923935692173dbaa64742e74663c61cc5c60df
Federico Fissore <f.fissore@arduino.cc>
2015-06-25 10:35:06 +0200

    CLI: fail if selected board does not exist. Fixes #3417

--- arduino-core/src/processing/app/helpers/CommandlineParser.java
+++ arduino-core/src/processing/app/helpers/CommandlineParser.java
@@ -244,7 +244,7 @@ public class CommandlineParser {
     }
 
     TargetBoard targetBoard = targetPlatform.getBoard(split[2]);
-    if (targetBoard == null) {
+    if (targetBoard == null || !targetBoard.getId().equals(split[2])) {
       BaseNoGui.showError(null, I18n.format(_("{0}: Unknown board"), split[2]), 3);
     }
 
