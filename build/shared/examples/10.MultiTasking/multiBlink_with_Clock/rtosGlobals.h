716b18f6b6e5c7365a01de2da2e7f9c3e8be275f
Rei Vilo <rei_vilo@yahoo.fr>
2015-06-30 12:57:22 +0200

    multiBlink_with_Clock example fixed

---
+++ build/shared/examples/10.MultiTasking/multiBlink_with_Clock/rtosGlobals.h
@@ -0,0 +1,46 @@
+///
+/// @file		rtosGlobals.h
+/// @brief		Global variables for Energia MT project
+///
+/// @details	<#details#>
+/// @n @a		Developed with [embedXcode+](http://embedXcode.weebly.com)
+///
+/// @author		Rei Vilo
+/// @author		http://embeddedcomputing.weebly.com
+/// @date		26/06/2015 14:48
+/// @version	<#version#>
+///
+/// @copyright	(c) Rei Vilo, 2015
+/// @copyright	CC = BY SA NC
+///
+/// @see		ReadMe.txt for references
+/// @n
+///
+
+
+// Core library
+#include "Energia.h"
+
+#ifndef rtosGlobals_h
+#define rtosGlobals_h
+
+// Include application, user and local libraries
+
+
+// Prototypes
+
+
+// Define variables and constants
+
+
+//// Add optional rtosSetup function
+//// Requires the updated main.template
+//void rtosSetup()
+//{
+//    pinMode(GREEN_LED, OUTPUT);
+//    digitalWrite(GREEN_LED, HIGH);
+//}
+
+
+
+#endif
