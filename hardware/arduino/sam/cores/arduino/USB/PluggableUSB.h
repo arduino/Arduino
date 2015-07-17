dfd411951201d85f3658e884ab99ec28310d6131
Martino Facchin <m.facchin@arduino.cc>
2015-07-17 12:16:38 +0200

    port PluggableUSB to sam core

---
+++ hardware/arduino/sam/cores/arduino/USB/PluggableUSB.h
@@ -0,0 +1,63 @@
+/*
+  PluggableUSB.h
+  Copyright (c) 2015 Arduino LLC
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef PUSB_h
+#define PUSB_h
+
+#include "USBAPI.h"
+#include <cstddef>
+
+#if defined(USBCON)
+
+typedef struct __attribute__((packed))
+{
+  bool (*setup)(USBSetup& setup, uint8_t i);
+  int (*getInterface)(uint8_t* interfaceNum);
+  int (*getDescriptor)(int8_t t);
+  int8_t numEndpoints;
+  int8_t numInterfaces;
+  uint8_t *endpointType;
+} PUSBCallbacks;
+
+typedef struct
+{
+  uint8_t interface;
+  uint8_t firstEndpoint;
+} PUSBReturn;
+
+class PUSBListNode {
+public:
+  PUSBListNode *next = NULL;
+  PUSBCallbacks *cb;
+  PUSBListNode(PUSBCallbacks *ncb) {cb = ncb;}
+};
+
+int8_t PUSB_AddFunction(PUSBListNode *node, uint8_t *interface);
+
+int PUSB_GetInterface(uint8_t* interfaceNum);
+
+int PUSB_GetDescriptor(int8_t t);
+
+bool PUSB_Setup(USBSetup& setup, uint8_t i);
+
+void PUSB_Begin();
+
+#endif
+
+#endif
--- hardware/arduino/sam/cores/arduino/USB/USBAPI.h
+++ hardware/arduino/sam/cores/arduino/USB/USBAPI.h
@@ -22,6 +22,10 @@
 #if defined __cplusplus
 
 #include "RingBuffer.h"
+#include "Stream.h"
+#include <cstddef>
+
+#define min(a, b)   Min(a, b)
 
 //================================================================================
 //================================================================================
@@ -88,97 +92,6 @@ extern Serial_ SerialUSB;
 
 //================================================================================
 //================================================================================
-//	Mouse
-
-#define MOUSE_LEFT 1
-#define MOUSE_RIGHT 2
-#define MOUSE_MIDDLE 4
-#define MOUSE_ALL (MOUSE_LEFT | MOUSE_RIGHT | MOUSE_MIDDLE)
-
-class Mouse_
-{
-private:
-	uint8_t _buttons;
-	void buttons(uint8_t b);
-public:
-	Mouse_(void);
-	void begin(void);
-	void end(void);
-	void click(uint8_t b = MOUSE_LEFT);
-	void move(signed char x, signed char y, signed char wheel = 0);
-	void press(uint8_t b = MOUSE_LEFT);		// press LEFT by default
-	void release(uint8_t b = MOUSE_LEFT);	// release LEFT by default
-	bool isPressed(uint8_t b = MOUSE_ALL);	// check all buttons by default
-};
-extern Mouse_ Mouse;
-
-//================================================================================
-//================================================================================
-//	Keyboard
-
-#define KEY_LEFT_CTRL		0x80
-#define KEY_LEFT_SHIFT		0x81
-#define KEY_LEFT_ALT		0x82
-#define KEY_LEFT_GUI		0x83
-#define KEY_RIGHT_CTRL		0x84
-#define KEY_RIGHT_SHIFT		0x85
-#define KEY_RIGHT_ALT		0x86
-#define KEY_RIGHT_GUI		0x87
-
-#define KEY_UP_ARROW		0xDA
-#define KEY_DOWN_ARROW		0xD9
-#define KEY_LEFT_ARROW		0xD8
-#define KEY_RIGHT_ARROW		0xD7
-#define KEY_BACKSPACE		0xB2
-#define KEY_TAB				0xB3
-#define KEY_RETURN			0xB0
-#define KEY_ESC				0xB1
-#define KEY_INSERT			0xD1
-#define KEY_DELETE			0xD4
-#define KEY_PAGE_UP			0xD3
-#define KEY_PAGE_DOWN		0xD6
-#define KEY_HOME			0xD2
-#define KEY_END				0xD5
-#define KEY_CAPS_LOCK		0xC1
-#define KEY_F1				0xC2
-#define KEY_F2				0xC3
-#define KEY_F3				0xC4
-#define KEY_F4				0xC5
-#define KEY_F5				0xC6
-#define KEY_F6				0xC7
-#define KEY_F7				0xC8
-#define KEY_F8				0xC9
-#define KEY_F9				0xCA
-#define KEY_F10				0xCB
-#define KEY_F11				0xCC
-#define KEY_F12				0xCD
-
-//	Low level key report: up to 6 keys and shift, ctrl etc at once
-typedef struct
-{
-	uint8_t modifiers;
-	uint8_t reserved;
-	uint8_t keys[6];
-} KeyReport;
-
-class Keyboard_ : public Print
-{
-private:
-	KeyReport _keyReport;
-	void sendReport(KeyReport* keys);
-public:
-	Keyboard_(void);
-	void begin(void);
-	void end(void);
-	virtual size_t write(uint8_t k);
-	virtual size_t press(uint8_t k);
-	virtual size_t release(uint8_t k);
-	virtual void releaseAll(void);
-};
-extern Keyboard_ Keyboard;
-
-//================================================================================
-//================================================================================
 //	Low level API
 
 typedef struct
@@ -189,16 +102,7 @@ typedef struct
 	uint8_t wValueH;
 	uint16_t wIndex;
 	uint16_t wLength;
-} Setup;
-
-//================================================================================
-//================================================================================
-//	HID 'Driver'
-
-int		HID_GetInterface(uint8_t* interfaceNum);
-int		HID_GetDescriptor(int i);
-bool	HID_Setup(Setup& setup);
-void	HID_SendReport(uint8_t id, const void* data, uint32_t len);
+} USBSetup;
 
 //================================================================================
 //================================================================================
@@ -206,7 +110,7 @@ void	HID_SendReport(uint8_t id, const void* data, uint32_t len);
 
 int		MSC_GetInterface(uint8_t* interfaceNum);
 int		MSC_GetDescriptor(int i);
-bool	MSC_Setup(Setup& setup);
+bool	MSC_Setup(USBSetup& setup);
 bool	MSC_Data(uint8_t rx,uint8_t tx);
 
 //================================================================================
@@ -216,7 +120,7 @@ bool	MSC_Data(uint8_t rx,uint8_t tx);
 int		CDC_GetInterface(uint8_t* interfaceNum);
 int		CDC_GetOtherInterface(uint8_t* interfaceNum);
 int		CDC_GetDescriptor(int i);
-bool	CDC_Setup(Setup& setup);
+bool	CDC_Setup(USBSetup& setup);
 
 //================================================================================
 //================================================================================
@@ -228,7 +132,7 @@ void USBD_InitControl(int end);
 int USBD_SendControl(uint8_t flags, const void* d, uint32_t len);
 int USBD_RecvControl(void* d, uint32_t len);
 int USBD_SendInterfaces(void);
-bool USBD_ClassInterfaceRequest(Setup& setup);
+bool USBD_ClassInterfaceRequest(USBSetup& setup);
 
 
 uint32_t USBD_Available(uint32_t ep);
