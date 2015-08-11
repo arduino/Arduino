6eba593e3b3ba2acc579c95df293342ffec8140d
Chuck Todd <ctodd@cableone.net>
2015-08-11 17:54:53 -0600

    Fix missing ')'
    bad transcription

--- hardware/arduino/avr/cores/arduino/Print.cpp
+++ hardware/arduino/avr/cores/arduino/Print.cpp
@@ -35,7 +35,7 @@ size_t Print::write(const uint8_t *buffer, size_t size)
 {
   size_t n = 0;
   while (size--) {
-    if(write(*buffer++) n++;
+    if(write(*buffer++)) n++;
     else break;
   }
   return n;
