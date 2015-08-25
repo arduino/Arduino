1609fbc61484c22eca9681ed392988d359c9da02
Eddy <est.git@online.de>
2015-08-25 21:47:31 +0200

    Ethernet use DNS in examples
    Set the DNS-Server as third parameter of Ethernet.begin if at least gateway is specified. Reference is Updated before.
    
    In BarometricPressureWebServer gateway and subnet wasn't used.
    
    Signed-off-by: Eddy <est.git@online.de>

--- libraries/Ethernet/examples/AdvancedChatServer/AdvancedChatServer.ino
+++ libraries/Ethernet/examples/AdvancedChatServer/AdvancedChatServer.ino
@@ -30,6 +30,7 @@ byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 };
 IPAddress ip(192, 168, 1, 177);
+IPAddress myDns(192, 168, 1, 1);
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 0, 0);
 
@@ -41,7 +42,7 @@ EthernetClient clients[4];
 
 void setup() {
   // initialize the ethernet device
-  Ethernet.begin(mac, ip, gateway, subnet);
+  Ethernet.begin(mac, ip, myDns, gateway, subnet);
   // start listening for clients
   server.begin();
   // Open serial communications and wait for port to open:
