f5c1084f5f009a6c6f6178c098abc36cf94ee828
Federico Fissore <f.fissore@arduino.cc>
2015-10-29 14:09:51 +0100

    Fixed wrong i18n function name

--- arduino-core/src/processing/app/Serial.java
+++ arduino-core/src/processing/app/Serial.java
@@ -114,7 +114,7 @@ public class Serial implements SerialPortEventListener {
       port.openPort();
       boolean res = port.setParams(irate, idatabits, stopbits, parity, true, true);
       if (!res) {
-        System.err.println(format(_("Error while setting serial port parameters: {0} {1} {2} {3}"),
+        System.err.println(format(tr("Error while setting serial port parameters: {0} {1} {2} {3}"),
                                   irate, iparity, idatabits, istopbits));
       }
       port.addEventListener(this);
