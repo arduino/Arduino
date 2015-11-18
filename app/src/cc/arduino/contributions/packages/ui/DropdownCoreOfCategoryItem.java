b450a2743e2974213d0e15b1a49abbc8faedea92
Federico Fissore <f.fissore@arduino.cc>
2015-11-18 17:27:35 +0100

    Allowing boards/libs types and categories to be translated. Fixes #3646

--- app/src/cc/arduino/contributions/packages/ui/DropdownCoreOfCategoryItem.java
+++ app/src/cc/arduino/contributions/packages/ui/DropdownCoreOfCategoryItem.java
@@ -35,6 +35,8 @@ import cc.arduino.contributions.ui.DropdownItem;
 
 import java.util.function.Predicate;
 
+import static processing.app.I18n.tr;
+
 public class DropdownCoreOfCategoryItem implements DropdownItem<ContributedPlatform> {
 
   private final String category;
@@ -44,7 +46,7 @@ public class DropdownCoreOfCategoryItem implements DropdownItem<ContributedPlatf
   }
 
   public String toString() {
-    return category;
+    return tr(category);
   }
 
   @Override
