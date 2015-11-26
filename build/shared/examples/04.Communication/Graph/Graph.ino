2f00c938a741819bd250547490ddd060505169a4
agdl <a.guadalupi@arduino.cc>
2015-11-26 11:33:27 +0100

    Fixed processing code for Processing 3
    As pointed out in #4214 I changed the processing code to make it working wit Processing 3

--- build/shared/examples/04.Communication/Graph/Graph.ino
+++ build/shared/examples/04.Communication/Graph/Graph.ino
@@ -44,71 +44,71 @@ void loop() {
  // Graphing sketch
 
 
- // This program takes ASCII-encoded strings
- // from the serial port at 9600 baud and graphs them. It expects values in the
- // range 0 to 1023, followed by a newline, or newline and carriage return
+// This program takes ASCII-encoded strings
+// from the serial port at 9600 baud and graphs them. It expects values in the
+// range 0 to 1023, followed by a newline, or newline and carriage return
 
- // Created 20 Apr 2005
- // Updated 18 Jan 2008
- // by Tom Igoe
- // This example code is in the public domain.
+// Created 20 Apr 2005
+// Updated 24 Nov 2015
+// by Tom Igoe
+// This example code is in the public domain.
 
- import processing.serial.*;
+import processing.serial.*;
 
- Serial myPort;        // The serial port
- int xPos = 1;         // horizontal position of the graph
+Serial myPort;        // The serial port
+int xPos = 1;         // horizontal position of the graph
+float inByte = 0;
 
- void setup () {
- // set the window size:
- size(400, 300);
+void setup () {
+  // set the window size:
+  size(400, 300);
 
- // List all the available serial ports
- // if using Processing 2.1 or later, use Serial.printArray()
- println(Serial.list());
+  // List all the available serial ports
+  // if using Processing 2.1 or later, use Serial.printArray()
+  println(Serial.list());
 
- // I know that the first port in the serial list on my mac
- // is always my  Arduino, so I open Serial.list()[0].
- // Open whatever port is the one you're using.
- myPort = new Serial(this, Serial.list()[0], 9600);
+  // I know that the first port in the serial list on my mac
+  // is always my  Arduino, so I open Serial.list()[0].
+  // Open whatever port is the one you're using.
+  myPort = new Serial(this, Serial.list()[0], 9600);
 
- // don't generate a serialEvent() unless you get a newline character:
- myPort.bufferUntil('\n');
+  // don't generate a serialEvent() unless you get a newline character:
+  myPort.bufferUntil('\n');
 
- // set inital background:
- background(0);
- }
- void draw () {
- // everything happens in the serialEvent()
- }
+  // set inital background:
+  background(0);
+}
+void draw () {
+  // draw the line:
+  stroke(127, 34, 255);
+  line(xPos, height, xPos, height - inByte);
 
- void serialEvent (Serial myPort) {
- // get the ASCII string:
- String inString = myPort.readStringUntil('\n');
+  // at the edge of the screen, go back to the beginning:
+  if (xPos >= width) {
+    xPos = 0;
+    background(0);
+  } else {
+    // increment the horizontal position:
+    xPos++;
+  }
+}
 
- if (inString != null) {
- // trim off any whitespace:
- inString = trim(inString);
- // convert to an int and map to the screen height:
- float inByte = float(inString);
- inByte = map(inByte, 0, 1023, 0, height);
 
- // draw the line:
- stroke(127,34,255);
- line(xPos, height, xPos, height - inByte);
+void serialEvent (Serial myPort) {
+  // get the ASCII string:
+  String inString = myPort.readStringUntil('\n');
 
- // at the edge of the screen, go back to the beginning:
- if (xPos >= width) {
- xPos = 0;
- background(0);
- }
- else {
- // increment the horizontal position:
- xPos++;
- }
- }
- }
+  if (inString != null) {
+    // trim off any whitespace:
+    inString = trim(inString);
+    // convert to an int and map to the screen height:
+    inByte = float(inString);
+    println(inByte);
+    inByte = map(inByte, 0, 1023, 0, height);
+  }
+}
 
- */
+*/
 
 /* Max/MSP v5 patch for this example
  ----------begin_max5_patcher----------
