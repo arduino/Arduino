283ccc150d3afec675c87d0c3da11e42a633b2d1
Matthijs Kooijman <matthijs@stdin.nl>
2015-12-08 19:44:57 +0100

    Print errors while reloading externally edited files
    These exceptions were silently dropped, which is pretty much never a
    good idea.

--- app/src/processing/app/Base.java
+++ app/src/processing/app/Base.java
@@ -614,7 +614,7 @@ public class Base {
           activeEditor.getCurrentTab().getTextArea().setCaretPosition(previousCaretPosition);
         }
       } catch (IOException e) {
-        // noop
+        System.err.println(e);
       }
     }
 
