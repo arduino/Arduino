63de1cccfb4a0a7d9246e32ccdafc2c0d985e24d
Martino Facchin <m.facchin@arduino.cc>
2016-02-11 16:06:55 +0100

    Avoid generating an exception if upload fails
    The current method of reporting upload errors is based on an exoteric combination of exceptions which makes return error code useless
    The Uploader.java message() implementation is too avrdude-dependant to allow easy portability since the upload tools are becoming a lot and very different
    
    With this commit we try to avoid exceptions and only use the external uploader's exit code to decide the status bar message.
    The message can be:
    - the last line containing "error" string (any case) or
    - the usual avrdude message parsing (to keep compatibility with translations)
    
    Needs testing with all platform and all supported upload tools

--- arduino-core/src/cc/arduino/packages/Uploader.java
+++ arduino-core/src/cc/arduino/packages/Uploader.java
@@ -130,13 +130,11 @@ public abstract class Uploader implements MessageConsumer {
       e.printStackTrace();
     }
 
-    if (error != null) {
-      RunnerException exception = new RunnerException(error);
-      exception.hideStackTrace();
-      throw exception;
-    }
-
     return result == 0;
+  }
+
+  public String getFailureMessage() {
+    return error;
   }
 
   public void message(String s) {
@@ -148,8 +146,9 @@ public abstract class Uploader implements MessageConsumer {
     System.err.print(s);
 
     // ignore cautions
-    if (s.contains("Error")) {
+    if (s.toLowerCase().contains("error")) {
       notFoundError = true;
+      error = s;
       return;
     }
     if (notFoundError) {
