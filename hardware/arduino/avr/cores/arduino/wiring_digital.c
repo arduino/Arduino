275a47da89df98468f60545ba5864f621511fd53
tico-tico <sergei.ivn@gmx.com>
2016-02-28 22:20:47 +0300

    save some bytes by replacing int with uint8_t in Prints, digitalRead() and analogWrite()

--- hardware/arduino/avr/cores/arduino/wiring_digital.c
+++ hardware/arduino/avr/cores/arduino/wiring_digital.c
@@ -162,7 +162,7 @@ void digitalWrite(uint8_t pin, uint8_t val)
 	SREG = oldSREG;
 }
 
-int digitalRead(uint8_t pin)
+uint8_t digitalRead(uint8_t pin)
 {
 	uint8_t timer = digitalPinToTimer(pin);
 	uint8_t bit = digitalPinToBitMask(pin);
