a86176dd8a2181738435b1ae1a4cc385f741804e
Martino Facchin <m.facchin@arduino.cc>
2016-03-02 10:08:25 +0100

    set a real pty for sending ssh commands

--- arduino-core/src/cc/arduino/packages/ssh/SSH.java
+++ arduino-core/src/cc/arduino/packages/ssh/SSH.java
@@ -58,6 +58,9 @@ public class SSH {
     try {
       channel = session.openChannel("exec");
       ((ChannelExec) channel).setCommand(command);
+      ((ChannelExec) channel).setAgentForwarding(true);
+      ((ChannelExec) channel).setPty(true);
+      ((ChannelExec) channel).setPtyType( "xterm" );
 
       channel.setInputStream(null);
 
