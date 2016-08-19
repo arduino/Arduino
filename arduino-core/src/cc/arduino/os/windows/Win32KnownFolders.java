cbf3cdcfbb1fcbee7eb08c41ba5d393ef96d649a
Cristian Maglie <c.maglie@arduino.cc>
2016-08-19 15:30:39 +0300

    Added Win32KnownFolders class

---
+++ arduino-core/src/cc/arduino/os/windows/Win32KnownFolders.java
@@ -0,0 +1,54 @@
+/*
+ * This file is part of Arduino.
+ *
+ * Copyright 2016 Arduino LLC (http://www.arduino.cc/)
+ *
+ * Arduino is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As a special exception, you may use this file as part of a free software
+ * library without restriction.  Specifically, if other files instantiate
+ * templates or use macros or inline functions from this file, or you compile
+ * this file and link it with other files to produce an executable, this
+ * file does not by itself cause the resulting executable to be covered by
+ * the GNU General Public License.  This exception does not however
+ * invalidate any other reasons why the executable file might be covered by
+ * the GNU General Public License.
+ */
+
+package cc.arduino.os.windows;
+
+import static com.sun.jna.platform.win32.KnownFolders.FOLDERID_Documents;
+import static com.sun.jna.platform.win32.KnownFolders.FOLDERID_LocalAppData;
+import static com.sun.jna.platform.win32.KnownFolders.FOLDERID_RoamingAppData;
+
+import java.io.File;
+
+import com.sun.jna.platform.win32.Shell32Util;
+
+public class Win32KnownFolders {
+
+  public static File getLocalAppDataFolder() {
+    return new File(Shell32Util.getKnownFolderPath(FOLDERID_LocalAppData));
+  }
+
+  public static File getRoamingAppDataFolder() {
+    return new File(Shell32Util.getKnownFolderPath(FOLDERID_RoamingAppData));
+  }
+
+  public static File getDocumentsFolder() {
+    return new File(Shell32Util.getKnownFolderPath(FOLDERID_Documents));
+  }
+
+}
