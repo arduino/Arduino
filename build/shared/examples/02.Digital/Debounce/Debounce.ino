332931e3a697f58a4f25106cc91c8f3521fa3d14
agdl <a.guadalupi@arduino.cc>
2016-08-31 16:29:40 +0200

    Fixed example comment

--- build/shared/examples/02.Digital/Debounce/Debounce.ino
+++ build/shared/examples/02.Digital/Debounce/Debounce.ino
@@ -40,8 +40,8 @@ int ledState = HIGH;         // the current state of the output pin
 int buttonState;             // the current reading from the input pin
 int lastButtonState = LOW;   // the previous reading from the input pin
 
-// the following variables are long's because the time, measured in miliseconds,
-// will quickly become a bigger number than can be stored in an int.
+// the following variables are unsigned long's because the time, measured in miliseconds,
+// will quickly become a bigger number than can't be stored in an int.
 unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
