7f41a5ece910b3f863ac94a30bbc2ef3f7e5ec86
agdl <a.guadalupi@arduino.cc>
2016-09-02 12:27:03 +0200

    Improved Blink example comment

--- build/shared/examples/01.Basics/Blink/Blink.ino
+++ build/shared/examples/01.Basics/Blink/Blink.ino
@@ -3,10 +3,11 @@
   Turns on an LED on for one second, then off for one second, repeatedly.
 
   Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO 
-  it is attached to digital pin 13, on MKR1000 on pin 6. 
-  If you're unsure what pin the on-board LED is connected to on your Arduino model, check
-  the documentation at http://www.arduino.cc
-
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN takes care 
+  of use the correct LED pin whatever is the board used.
+  If you want to know what pin the on-board LED is connected to on your Arduino model, check
+  the Technical Specs of your board  at https://www.arduino.cc/en/Main/Products
+  
   This example code is in the public domain.
 
   modified 8 May 2014
