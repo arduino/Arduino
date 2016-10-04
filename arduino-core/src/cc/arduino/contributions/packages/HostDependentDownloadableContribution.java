481fd94ac64f3031d82fd8f2612eafbbff4b5c65
Kyle Evans <kevans91@ksu.edu>
2016-10-03 23:37:27 -0500

    Add FreeBSD Platform Tag -- supports x86_64-freebsd, arm*-freebsd, and i386-freebsd with optional extension for tagging specific release (e.g. freebsd11)

--- arduino-core/src/cc/arduino/contributions/packages/HostDependentDownloadableContribution.java
+++ arduino-core/src/cc/arduino/contributions/packages/HostDependentDownloadableContribution.java
@@ -79,6 +79,16 @@ public abstract class HostDependentDownloadableContribution extends Downloadable
       }
     }
 
+    if (osName.contains("FreeBSD")) {
+      if (osArch.contains("amd64")) {
+        return host.matches("x86_64-freebsd[0-9]*");
+      } else if (osArch.contains("arm")) {
+        return host.matches("arm.*-freebsd[0-9]*");
+      } else {
+        return host.matches("i386-freebsd[0-9]*");
+      }
+    }
+
     return false;
   }
 }
