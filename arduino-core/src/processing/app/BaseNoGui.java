d115f44a31a287230030702280e5c26d8663e75b
Martino Facchin <m.facchin@arduino.cc>
2016-12-21 16:12:44 +0100

    Update IDE version to 1.8.0

--- arduino-core/src/processing/app/BaseNoGui.java
+++ arduino-core/src/processing/app/BaseNoGui.java
@@ -38,9 +38,9 @@ import static processing.app.helpers.filefilters.OnlyDirs.ONLY_DIRS;
 public class BaseNoGui {
 
   /** Version string to be used for build */
-  public static final int REVISION = 10614;
+  public static final int REVISION = 10800;
   /** Extended version string displayed on GUI */
-  public static final String VERSION_NAME = "1.6.14";
+  public static final String VERSION_NAME = "1.8.0";
   public static final String VERSION_NAME_LONG;
 
   // Current directory to use for relative paths specified on the
