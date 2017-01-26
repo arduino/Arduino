5d602f226394ed513655693ff96d546ade4aca4c
Cristian Maglie <c.maglie@arduino.cc>
2017-01-26 18:00:53 +0100

    LibraryInstaller now autodetects if a library is being replaced
    It's no more required to pass this information from outside,
    just library that is being installed is now sufficient.

--- app/src/processing/app/Base.java
+++ app/src/processing/app/Base.java
@@ -362,7 +362,7 @@ public class Base {
         if (selected.isReadOnly()) {
           libraryInstaller.remove(installed, progressListener);
         } else {
-          libraryInstaller.install(selected, installed, progressListener);
+          libraryInstaller.install(selected, progressListener);
         }
       }
 
