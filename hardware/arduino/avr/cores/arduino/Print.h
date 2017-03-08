c6bac064065b667bf8099ed0131742b1f111cb94
Cristian Maglie <c.maglie@arduino.cc>
2017-03-08 13:30:33 +0100

    AVR: Prevent warnings if BIN is previously defined
    Fix #4784
    Close #4791

--- hardware/arduino/avr/cores/arduino/Print.h
+++ hardware/arduino/avr/cores/arduino/Print.h
@@ -29,6 +29,9 @@
 #define DEC 10
 #define HEX 16
 #define OCT 8
+#ifdef BIN // Prevent warnings if BIN is previously defined in "iotnx4.h" or similar
+#undef BIN
+#endif
 #define BIN 2
 
 class Print
