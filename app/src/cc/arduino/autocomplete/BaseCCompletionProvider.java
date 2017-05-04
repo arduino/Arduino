37c38588f7f12d372b93e457a126c53908b1d4fd
ricardo.jl.rufino@gmail.com <ricardo.jl.rufino@gmail.com>
2017-05-03 21:53:29 -0300

    Refactoring autocomplete structure

---
+++ app/src/cc/arduino/autocomplete/BaseCCompletionProvider.java
@@ -0,0 +1,17 @@
+package cc.arduino.autocomplete;
+
+import org.fife.ui.autocomplete.DefaultCompletionProvider;
+
+/**
+ * Base completion provider for C/C++.
+ * @author Ricardo JL Rufino (ricardo@criativasoft.com.br)
+ * @date 28/04/2017
+ */
+public class BaseCCompletionProvider extends DefaultCompletionProvider{
+ 
+  @Override
+  protected boolean isValidChar(char ch) {
+    return super.isValidChar(ch) || '.' == ch || '>' == ch  || '-' == ch  || '<' == ch || '#' == ch || ':' == ch /**|| getParameterListStart() == ch */;
+  }
+
+}
