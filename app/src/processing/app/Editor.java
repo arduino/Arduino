f385cbff951a0801971c01d3cc5a10d1fa356bc6
Martino Facchin <m.facchin@arduino.cc>
2017-09-13 11:53:54 +0200

    Fix Increase/Decrease font size for different keyboard layouts

--- app/src/processing/app/Editor.java
+++ app/src/processing/app/Editor.java
@@ -1419,7 +1419,7 @@ public class Editor extends JFrame implements RunnerListener, FocusListener {
 
     menu.addSeparator();
 
-    JMenuItem increaseFontSizeItem = newJMenuItem(tr("Increase Font Size"), '+');
+    JMenuItem increaseFontSizeItem = newJMenuItem(tr("Increase Font Size"), KeyEvent.VK_PLUS);
     increaseFontSizeItem.addActionListener(new ActionListener() {
         public void actionPerformed(ActionEvent e) {
           base.handleFontSizeChange(1);
@@ -1427,7 +1427,7 @@ public class Editor extends JFrame implements RunnerListener, FocusListener {
     });
     menu.add(increaseFontSizeItem);
 
-    JMenuItem decreaseFontSizeItem = newJMenuItem(tr("Decrease Font Size"), '-');
+    JMenuItem decreaseFontSizeItem = newJMenuItem(tr("Decrease Font Size"), KeyEvent.VK_MINUS);
     decreaseFontSizeItem.addActionListener(new ActionListener() {
         public void actionPerformed(ActionEvent e) {
           base.handleFontSizeChange(-1);
