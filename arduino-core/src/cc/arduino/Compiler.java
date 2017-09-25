545b5276316a1bbfc0857b823f40c95c54bc9803
Cristian Maglie <c.maglie@arduino.cc>
2017-09-25 15:46:56 +0200

    Removed unused import

--- arduino-core/src/cc/arduino/Compiler.java
+++ arduino-core/src/cc/arduino/Compiler.java
@@ -55,7 +55,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static processing.app.I18n.tr;
 
