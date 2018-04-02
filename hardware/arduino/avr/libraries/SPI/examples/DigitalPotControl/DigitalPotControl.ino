f7a395fbdb22f72c3b2263f68bae7872e56bb73d
SimonePDA <majocchi@aquariusedizioni.it>
2018-04-02 19:18:50 +0200

    Update DigitalPotControl.ino
    Fixing SPI communication with a delay as pointe out in #6395

--- hardware/arduino/avr/libraries/SPI/examples/DigitalPotControl/DigitalPotControl.ino
+++ hardware/arduino/avr/libraries/SPI/examples/DigitalPotControl/DigitalPotControl.ino
@@ -63,9 +63,11 @@ void loop() {
 void digitalPotWrite(int address, int value) {
   // take the SS pin low to select the chip:
   digitalWrite(slaveSelectPin, LOW);
+  delay(100);
   //  send in the address and value via SPI:
   SPI.transfer(address);
   SPI.transfer(value);
+  delay(100);
   // take the SS pin high to de-select the chip:
   digitalWrite(slaveSelectPin, HIGH);
 }
