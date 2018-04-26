756c995fa28f44ff5a40672e970a58a8115ed8ab
Ayo Adebisi <35472047+ayohenry@users.noreply.github.com>
2018-04-26 11:03:08 -0400

    Revert String.toFloat()

--- hardware/arduino/avr/cores/arduino/WString.cpp
+++ hardware/arduino/avr/cores/arduino/WString.cpp
@@ -20,7 +20,6 @@
 */
 
 #include "WString.h"
-#include <limits>
 
 /*********************************************/
 /*  Constructors                             */
@@ -747,6 +746,5 @@ float String::toFloat(void) const
 double String::toDouble(void) const
 {
 	if (buffer) return atof(buffer);
-	/* Return NaN upon failure as oppose to 0.0 */
-	return std::numeric_limits<double>::quiet_NaN();
+	return 0.0;
 }
