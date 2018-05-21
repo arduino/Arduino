2eb0fccf09ae20877cf90119158e94fae37e96f9
Edward Betts <edward@4angle.com>
2018-05-21 13:29:11 +0100

    Correct a spelling mistake.

--- libraries/TFT/src/utility/Adafruit_GFX.cpp
+++ libraries/TFT/src/utility/Adafruit_GFX.cpp
@@ -160,7 +160,7 @@ void Adafruit_GFX::fillCircleHelper(int16_t x0, int16_t y0, int16_t r,
   }
 }
 
-// Bresenham's algorithm - thx wikpedia
+// Bresenham's algorithm - thx wikipedia
 void Adafruit_GFX::drawLine(int16_t x0, int16_t y0, 
 			    int16_t x1, int16_t y1, 
 			    uint16_t color) 
