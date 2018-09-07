arduino/Arduino

commit b3d01d82810b5ac895623bdaebb647bf82bee1e4

Moved install-related fields out of DownloadableContribution
Those fields have a slightly different meaning on each object that
extends DownloadableContribution and having them grouped in
DownloadableContribution only increase confusion in change of a
(very) tiny code reuse.

Moreover:

- the `readOnly` field has been renamed to `builtIn`
- predicates have been replaced by lambdas
- DownloadableContributionBuiltInAtTheBottomComparator has been replaced
  with a singleton instance

arduino-core/src/cc/arduino/contributions/packages/ContributedPlatform.java
@@ -32,6 +32,7 @@ package cc.arduino.contributions.packages;
 import cc.arduino.contributions.DownloadableContribution;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import java.io.File;
 import java.util.*;
 
 public abstract class ContributedPlatform extends DownloadableContribution {
@@ -52,6 +53,42 @@ public abstract class ContributedPlatform extends DownloadableContribution {
   public abstract List<ContributedBoard> getBoards();
 
   public abstract ContributedHelp getHelp();
+
+  private boolean installed;
+
+  public boolean isInstalled() {
+    return installed;
+  }
+
+  public void setInstalled(boolean installed) {
+    this.installed = installed;
+  }
+
+  private File installedFolder;
+
+  public File getInstalledFolder() {
+    return installedFolder;
+  }
+
+  public void setInstalledFolder(File installedFolder) {
+    this.installedFolder = installedFolder;
+  }
+
+  private boolean builtIn;
+
+  public boolean isBuiltIn() {
+    return builtIn;
+  }
+
+  public void setBuiltIn(boolean builtIn) {
+    this.builtIn = builtIn;
+  }
+
+  public static final Comparator<ContributedPlatform> BUILTIN_AS_LAST = (x, y) -> {
+    int px = x.isBuiltIn() ? 1 : -1;
+    int py = y.isBuiltIn() ? 1 : -1;
+    return py - px;
+  };
 
   private Map<ContributedToolReference, ContributedTool> resolvedToolReferences;
 

app/src/cc/arduino/contributions/BuiltInCoreIsNewerCheck.java
@@ -29,9 +29,6 @@
 
 package cc.arduino.contributions;
 
-import cc.arduino.contributions.filters.BuiltInPredicate;
-import cc.arduino.contributions.filters.InstalledPredicate;
-import cc.arduino.contributions.packages.ContributedPackage;
 import cc.arduino.contributions.packages.ContributedPlatform;
 import processing.app.Base;
 import processing.app.BaseNoGui;
@@ -39,8 +36,8 @@ import processing.app.I18n;
 import processing.app.PreferencesData;
 
 import javax.swing.*;
-import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static processing.app.I18n.tr;
@@ -67,13 +64,21 @@ public class BuiltInCoreIsNewerCheck implements Runnable {
       return;
     }
 
-    List<ContributedPlatform> contributedPlatforms = BaseNoGui.indexer.getPackages().stream().map(ContributedPackage::getPlatforms).flatMap(Collection::stream).collect(Collectors.toList());
+    List<ContributedPlatform> contributedPlatforms = BaseNoGui.indexer
+        .getPackages().stream() //
+        .map(pack -> pack.getPlatforms()) //
+        .flatMap(platfs -> platfs.stream()) //
+        .collect(Collectors.toList());
 
-    List<ContributedPlatform> installedBuiltInPlatforms = contributedPlatforms.stream().filter(new InstalledPredicate()).filter(new BuiltInPredicate()).collect(Collectors.toList());
-    if (installedBuiltInPlatforms.size() != 1) {
+    Optional<ContributedPlatform> mayInstalledBuiltIn = contributedPlatforms
+        .stream() //
+        .filter(p -> p.isInstalled()) //
+        .filter(p -> p.isBuiltIn()) //
+        .findFirst();
+    if (!mayInstalledBuiltIn.isPresent()) {
       return;
     }
-    final ContributedPlatform installedBuiltIn = installedBuiltInPlatforms.get(0);
+    final ContributedPlatform installedBuiltIn = mayInstalledBuiltIn.get();
 
     ContributedPlatform installedNotBuiltIn = BaseNoGui.indexer.getInstalled(installedBuiltIn.getParentPackage().getName(), installedBuiltIn.getArchitecture());
     if (installedNotBuiltIn == null) {

app/src/cc/arduino/contributions/packages/ui/ContributedPlatformReleases.java
@@ -34,8 +34,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import cc.arduino.contributions.DownloadableContributionBuiltInAtTheBottomComparator;
-import cc.arduino.contributions.filters.InstalledPredicate;
 import cc.arduino.contributions.packages.ContributedPackage;
 import cc.arduino.contributions.packages.ContributedPlatform;
 
@@ -72,10 +70,9 @@ public class ContributedPlatformReleases {
 
   public ContributedPlatform getInstalled() {
     List<ContributedPlatform> installedReleases = releases.stream()
-        .filter(new InstalledPredicate()).collect(Collectors.toList());
-    Collections
-        .sort(installedReleases,
-              new DownloadableContributionBuiltInAtTheBottomComparator());
+        .filter(p -> p.isInstalled()) //
+        .collect(Collectors.toList());
+    Collections.sort(installedReleases, ContributedPlatform.BUILTIN_AS_LAST);
 
     if (installedReleases.isEmpty()) {
       return null;

app/src/cc/arduino/contributions/packages/ui/ContributedPlatformTableCellEditor.java
@@ -42,8 +42,6 @@ import javax.swing.JTable;
 
 import cc.arduino.contributions.DownloadableContributionVersionComparator;
 import cc.arduino.contributions.VersionComparator;
-import cc.arduino.contributions.filters.BuiltInPredicate;
-import cc.arduino.contributions.filters.InstalledPredicate;
 import cc.arduino.contributions.packages.ContributedPlatform;
 import cc.arduino.contributions.ui.InstallerTableCell;
 import cc.arduino.utils.ReverseComparator;
@@ -86,11 +84,13 @@ public class ContributedPlatformTableCellEditor extends InstallerTableCell {
     final ContributedPlatform installed = value.getInstalled();
 
     List<ContributedPlatform> releases = new LinkedList<>(value.releases);
-    List<ContributedPlatform> uninstalledReleases = releases.stream()
-        .filter(new InstalledPredicate().negate()).collect(Collectors.toList());
+    List<ContributedPlatform> uninstalledReleases = releases.stream() //
+        .filter(p -> !p.isInstalled()) //
+        .collect(Collectors.toList());
 
-    List<ContributedPlatform> installedBuiltIn = releases.stream()
-        .filter(new InstalledPredicate()).filter(new BuiltInPredicate())
+    List<ContributedPlatform> installedBuiltIn = releases.stream() //
+        .filter(p -> p.isInstalled()) //
+        .filter(p -> p.isBuiltIn()) //
         .collect(Collectors.toList());
 
     if (installed != null && !installedBuiltIn.contains(installed)) {

app/src/cc/arduino/contributions/packages/ui/ContributedPlatformTableCellJPanel.java
@@ -170,7 +170,7 @@ public class ContributedPlatformTableCellJPanel extends JPanel {
       upgradable = false;
     } else {
       installable = false;
-      removable = !installed.isReadOnly() && !hasBuiltInRelease;
+      removable = !installed.isBuiltIn() && !hasBuiltInRelease;
       upgradable = new DownloadableContributionVersionComparator()
           .compare(selected, installed) > 0;
     }
@@ -187,7 +187,7 @@ public class ContributedPlatformTableCellJPanel extends JPanel {
 
     String desc = "<html><body>";
     desc += "<b>" + selected.getName() + "</b>";
-    if (installed != null && installed.isReadOnly()) {
+    if (installed != null && installed.isBuiltIn()) {
       desc += " Built-In ";
     }
 

app/src/cc/arduino/contributions/packages/ui/ContributionManagerUI.java
@@ -72,7 +72,7 @@ public class ContributionManagerUI extends InstallerJDialog {
       @Override
       protected void onInstall(ContributedPlatform selected,
                                ContributedPlatform installed) {
-        if (selected.isReadOnly()) {
+        if (selected.isBuiltIn()) {
           onRemovePressed(installed, false);
         } else {
           onInstallPressed(selected, installed);
@@ -166,7 +166,7 @@ public class ContributionManagerUI extends InstallerJDialog {
       List<String> errors = new LinkedList<>();
       try {
         setProgressVisible(true, tr("Installing..."));
-        if (platformToRemove != null && !platformToRemove.isReadOnly()) {
+        if (platformToRemove != null && !platformToRemove.isBuiltIn()) {
           errors.addAll(installer.remove(platformToRemove));
         }
         errors.addAll(installer.install(platformToInstall, this::setProgress));

app/src/processing/app/Base.java
@@ -320,11 +320,11 @@ public class Base {
 
       ContributedPlatform installed = indexer.getInstalled(boardToInstallParts[0], boardToInstallParts[1]);
 
-      if (!selected.isReadOnly()) {
+      if (!selected.isBuiltIn()) {
         contributionInstaller.install(selected, progressListener);
       }
 
-      if (installed != null && !installed.isReadOnly()) {
+      if (installed != null && !installed.isBuiltIn()) {
         contributionInstaller.remove(installed);
       }
 

arduino-core/src/cc/arduino/contributions/DownloadableContribution.java
@@ -35,13 +35,6 @@ import java.io.File;
 
 public abstract class DownloadableContribution {
 
-  // XXX: maybe installed fields should not be here but in UserLibrary and ContributedPlatform?
-  private boolean installed;
-  private File installedFolder;
-
-  private boolean downloaded;
-  private File downloadedFile;
-
   public abstract String getUrl();
 
   public abstract String getVersion();
@@ -52,6 +45,8 @@ public abstract class DownloadableContribution {
 
   public abstract String getArchiveFileName();
 
+  private boolean downloaded;
+
   public boolean isDownloaded() {
     return downloaded;
   }
@@ -59,6 +54,8 @@ public abstract class DownloadableContribution {
   public void setDownloaded(boolean downloaded) {
     this.downloaded = downloaded;
   }
+
+  private File downloadedFile;
 
   public File getDownloadedFile() {
     return downloadedFile;
@@ -68,46 +65,11 @@ public abstract class DownloadableContribution {
     this.downloadedFile = downloadedFile;
   }
 
-  public boolean isInstalled() {
-    return installed;
-  }
-
-  public void setInstalled(boolean installed) {
-    this.installed = installed;
-  }
-
-  public File getInstalledFolder() {
-    return installedFolder;
-  }
-
-  public void setInstalledFolder(File installedFolder) {
-    this.installedFolder = installedFolder;
-  }
-
-  private boolean readOnly;
-
-  public boolean isReadOnly() {
-    return readOnly;
-  }
-
-  public void setReadOnly(boolean readOnly) {
-    this.readOnly = readOnly;
-  }
-
   public String getParsedVersion() {
     Version version = VersionHelper.valueOf(getVersion());
     if (version == null) {
       return null;
     }
     return version.toString();
-  }
-
-  @Override
-  public String toString() {
-    String res = "";
-    if (installed) {
-      res += "installed on " + installedFolder.getAbsolutePath() + " (" + getSize() + " bytes)";
-    }
-    return res;
   }
 }

commit 2f2987a4d5d449688653272ac979bebcc2bdf7b1

Regression: fixed wrong BUILTIN_AS_LAST comparator
This bug has been introduced with b3d01d82810b5ac895623bdaebb647bf82bee1e4

Fix #7973

arduino-core/src/cc/arduino/contributions/packages/ContributedPlatform.java
@@ -87,7 +87,7 @@ public abstract class ContributedPlatform extends DownloadableContribution {
   public static final Comparator<ContributedPlatform> BUILTIN_AS_LAST = (x, y) -> {
     int px = x.isBuiltIn() ? 1 : -1;
     int py = y.isBuiltIn() ? 1 : -1;
-    return py - px;
+    return px - py;
   };
 
   private Map<ContributedToolReference, ContributedTool> resolvedToolReferences;