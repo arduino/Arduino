9c4d52c50a4ef3f9f62b0602803cb00dfd07cd85
Cristian Maglie <c.maglie@arduino.cc>
2018-09-10 14:45:54 +0200

    Release 1.8.7

--- arduino-core/src/processing/app/BaseNoGui.java
+++ arduino-core/src/processing/app/BaseNoGui.java
@@ -42,9 +42,9 @@ import static processing.app.helpers.filefilters.OnlyDirs.ONLY_DIRS;
 public class BaseNoGui {
 
   /** Version string to be used for build */
-  public static final int REVISION = 10806;
+  public static final int REVISION = 10807;
   /** Extended version string displayed on GUI */
-  public static final String VERSION_NAME = "1.8.6";
+  public static final String VERSION_NAME = "1.8.7";
   public static final String VERSION_NAME_LONG;
 
   // Current directory to use for relative paths specified on the
