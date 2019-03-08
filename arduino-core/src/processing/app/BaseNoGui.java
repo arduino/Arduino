ddd876ca4e47b158cd3e643ad018f2f7d5ed4bda
Martino Facchin <m.facchin@arduino.cc>
2019-03-08 11:24:51 +0100

    Make Recently used boards size configurable from preferences

--- arduino-core/src/processing/app/BaseNoGui.java
+++ arduino-core/src/processing/app/BaseNoGui.java
@@ -937,7 +937,7 @@ public class BaseNoGui {
     if (!recentlyUsedBoards.contains(targetBoard)) {
       recentlyUsedBoards.add(targetBoard);
     }
-    if (recentlyUsedBoards.size() > 4) {
+    if (recentlyUsedBoards.size() > PreferencesData.getInteger("editor.recent_boards.size", 4)) {
       recentlyUsedBoards.remove();
     }
   }
