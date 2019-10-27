83bd8282486b0a4b3c2ed379d8960568191a62c6
Lapo Luchini <lapo@lapo.it>
2019-10-27 11:19:30 +0100

    Improve the blinking logic
    Blink on exact interval, avoid accumulating delays in case we miss the right millisec.
    Also avoids a subtraction in the inner loop (and could be slightly more clear to understand?).

--- build/shared/examples/02.Digital/BlinkWithoutDelay/BlinkWithoutDelay.ino
+++ build/shared/examples/02.Digital/BlinkWithoutDelay/BlinkWithoutDelay.ino
@@ -22,6 +22,8 @@
   by Scott Fitzgerald
   modified 9 Jan 2017
   by Arturo Guadalupi
+  modified 28 Sep 2019
+  by Lapo Luchini
 
   This example code is in the public domain.
 
@@ -30,16 +32,14 @@
 
 // constants won't change. Used here to set a pin number:
 const int ledPin =  LED_BUILTIN;// the number of the LED pin
+const long interval = 1000;     // interval at which to blink (milliseconds)
 
 // Variables will change:
 int ledState = LOW;             // ledState used to set the LED
 
 // Generally, you should use "unsigned long" for variables that hold time
 // The value will quickly become too large for an int to store
-unsigned long previousMillis = 0;        // will store last time LED was updated
-
-// constants won't change:
-const long interval = 1000;           // interval at which to blink (milliseconds)
+unsigned long nextMillis = millis();  // next time the LED should be updated
 
 void setup() {
   // set the digital pin as output:
@@ -49,14 +49,14 @@ void setup() {
 void loop() {
   // here is where you'd put code that needs to be running all the time.
 
-  // check to see if it's time to blink the LED; that is, if the difference
-  // between the current time and last time you blinked the LED is bigger than
-  // the interval at which you want to blink the LED.
+  // check to see if it's time to blink the LED; that is, if the
+  // current time is bigger that the next time it should blink
   unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= interval) {
-    // save the last time you blinked the LED
-    previousMillis = currentMillis;
+  if (currentMillis >= nextMillis) {
+    int delta = currentMillis - nextMillis;
+    // set the next blink time
+    nextMillis += interval;
 
     // if the LED is off turn it on and vice-versa:
     if (ledState == LOW) {
