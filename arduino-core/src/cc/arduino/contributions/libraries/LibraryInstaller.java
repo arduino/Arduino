2fbd8fb9e9deccb55d065ba6de0382180bb1f346
Cristian Maglie <c.maglie@arduino.cc>
2019-12-06 15:05:27 +0100

    Removed, no more needed, LibraryInstaller::updateIndex method

--- arduino-core/src/cc/arduino/contributions/libraries/LibraryInstaller.java
+++ arduino-core/src/cc/arduino/contributions/libraries/LibraryInstaller.java
@@ -65,50 +65,6 @@ public class LibraryInstaller {
     this.platform = platform;
   }
 
-  public synchronized void updateIndex(ProgressListener progressListener) throws Exception {
-  /* 
-    final MultiStepProgress progress = new MultiStepProgress(3);
-    DownloadableContributionsDownloader downloader = new DownloadableContributionsDownloader(BaseNoGui.librariesIndexer.getStagingFolder());
-    // Step 1: Download index
-    File outputFile = BaseNoGui.librariesIndexer.getIndexFile();
-    // Create temp files
-    String signatureFileName = FilenameUtils.getName(new URL(Constants.LIBRARY_INDEX_URL).getPath());
-    File libraryIndexTemp = File.createTempFile(signatureFileName, ".tmp");
-    final URL libraryURL = new URL(Constants.LIBRARY_INDEX_URL);
-    final URL libraryGzURL = new URL(Constants.LIBRARY_INDEX_URL_GZ);
-    final String statusText = tr("Downloading libraries index...");
-    try {
-      GZippedJsonDownloader gZippedJsonDownloader = new GZippedJsonDownloader(downloader, libraryURL, libraryGzURL);
-      gZippedJsonDownloader.download(libraryIndexTemp, progress, statusText, progressListener, true);
-    } catch (InterruptedException e) {
-      // Download interrupted... just exit
-      return;
-    }
-    progress.stepDone();
-
-    URL signatureUrl = new URL(libraryURL.toString() + ".sig");
-    if (downloader.verifyDomain(signatureUrl)) {
-      if (downloader.checkSignature(progress, signatureUrl, progressListener, signatureVerifier, statusText, libraryIndexTemp)) {
-        // Replace old index with the updated one
-        if (libraryIndexTemp.length() > 0) {
-          Files.move(libraryIndexTemp.toPath(), outputFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-        }
-      } else {
-        FileDownloader.invalidateFiles(libraryGzURL, libraryURL, signatureUrl);
-        log.error("Fail to verify the signature of {} the cached files have been removed", libraryURL);
-      }
-    } else {
-      log.info("The domain is not selected to verify the signature. library index: {}", signatureUrl);
-    }
-
-    // Step 2: Parse index
-    BaseNoGui.librariesIndexer.regenerateIndex();
-
-    // Step 3: Rescan index
-    rescanLibraryIndex(progress, progressListener);
- */
-  }
-
   public void install(ContributedLibraryRelease lib, ProgressListener progressListener) throws Exception {
     ArrayList<ContributedLibraryRelease> libs = new ArrayList<>();
     libs.add(lib);
