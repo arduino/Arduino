9ba24b7931d0d904e349edc3051d6ab5368ebb57
MisterAwesome23 <36367483+MisterAwesome23@users.noreply.github.com>
2020-03-27 04:10:11 +0530

    Fixing incorrect file upload.

--- build/shared/examples/02.Digital/SimpleSiren/SimpleSiren.ino
+++ build/shared/examples/02.Digital/SimpleSiren/SimpleSiren.ino
@@ -1,37 +1,37 @@
 /*
-  Button
+  SimpleSiren
 
-  Turns on and off a light emitting diode(LED) connected to digital pin 13,
+  Description:
+  Haven't we all used horns / sirens. Just a touch on switch and siren blows.
+  Just a tap back and its stops honking. This library provides just this.
+  A bare minimum framework simulating a siren functioning.
+
+  A buzzer which is connected to digital pin 13 is blown,
   when pressing a pushbutton attached to pin 2.
 
   The circuit:
-  - LED attached from pin 13 to ground
+  - Buzzer attached from pin 13 to ground
   - pushbutton attached to pin 2 from +5V
   - 10K resistor attached to pin 2 from ground
 
-  - Note: on most Arduinos there is already an LED on the board
-    attached to pin 13.
+  - Note: on most Arduinos do NOT come with a Buzzer on the board
+    hence has to be attached to pin 13.
 
-  created 2005
-  by DojoDave <http://www.0j0.org>
-  modified 30 Aug 2011
-  by Tom Igoe
+  created March 2020,
+  by Manthan Admane
 
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Button
 */
 
 // constants won't change. They're used here to set pin numbers:
 const int buttonPin = 2;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
+const int buzzerPin =  13;      // the number of the Buzzer pin
 
 // variables will change:
 int buttonState = 0;         // variable for reading the pushbutton status
 
 void setup() {
-  // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
+  // initialize the Buzzer pin as an output:
+  pinMode(buzzerPin, OUTPUT);
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
 }
@@ -42,10 +42,10 @@ void loop() {
 
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
   if (buttonState == HIGH) {
-    // turn LED on:
-    digitalWrite(ledPin, HIGH);
+    // turn Buzzer on:
+    digitalWrite(buzzerPin, HIGH);
   } else {
-    // turn LED off:
-    digitalWrite(ledPin, LOW);
+    // turn Buzzer off:
+    digitalWrite(buzzerPin, LOW);
   }
 }
