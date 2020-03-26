864cea63bbb17269d44dea2e378d5baaa60429e4
Cristian Maglie <c.maglie@arduino.cc>
2020-03-26 18:40:07 +0100

    Added other helper functions to download data from URLs

--- app/src/processing/app/UpdateCheck.java
+++ app/src/processing/app/UpdateCheck.java
@@ -22,18 +22,27 @@
 
 package processing.app;
 
-import org.apache.commons.compress.utils.IOUtils;
-import processing.app.legacy.PApplet;
+import static processing.app.I18n.tr;
 
-import javax.swing.*;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
-import static processing.app.I18n.tr;
+import javax.swing.JOptionPane;
+
+import org.apache.commons.compress.utils.IOUtils;
+
+import processing.app.legacy.PApplet;
 
 
 /**
@@ -89,7 +98,7 @@ public class UpdateCheck implements Runnable {
                         System.getProperty("os.version") + "\t" +
                         System.getProperty("os.arch"), "UTF-8");
       
-      int latest = readInt("https://www.arduino.cc/latest.txt?" + info);
+      int latest = readIntFromURL("https://www.arduino.cc/latest.txt?" + info);
 
       String prompt =
         tr("A new version of Arduino is available,\n" +
@@ -118,14 +127,24 @@ public class UpdateCheck implements Runnable {
   }
 
 
-  protected int readInt(String filename) throws IOException {
-    URL url = new URL(filename);
-    BufferedReader reader = null;
-    try {
-      reader = new BufferedReader(new InputStreamReader(url.openStream()));
-      return Integer.parseInt(reader.readLine());
-    } finally {
-      IOUtils.closeQuietly(reader);
+  protected int readIntFromURL(String _url) throws Exception {
+    List<String> lines = readFileFromURL(_url);
+    return Integer.parseInt(lines.get(0));
+  }
+
+  protected List<String> readFileFromURL(String _url) throws IOException {
+    URL url = new URL(_url);
+    try (BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));) {
+      return in.lines().collect(Collectors.toList());
+    }
+  }
+
+  protected void downloadFileFromURL(String _url, File dest) throws IOException {
+    URL url = new URL(_url);
+    try (InputStream in = url.openStream()) {
+      try (FileOutputStream out = new FileOutputStream(dest)) {
+        IOUtils.copy(in, out);
+      }
     }
   }
 }
