715b3eb641bd102dde3ffee8db27564e1e8f5bf3
shawayusx <ayushshaw456@gmail.com>
2020-03-28 23:13:49 +0530

    Adds i18n

--- app/src/processing/app/Editor.java
+++ app/src/processing/app/Editor.java
@@ -507,17 +507,6 @@ public class Editor extends JFrame implements RunnerListener {
 
 
   private void buildMenuBar() {
-  fileMenu.setMnemonic(trm("File"));
-  public static int trm(String s) {
-    try {
-      if (i18n == null)
-         return s.charAt(0);
-      else
-         return i18n.getString(s).charAt(0);
-        } catch (MissingResourceException e) {
-               return s.charAt(0);
-          }
-  }
     JMenuBar menubar = new JMenuBar();
     final JMenu fileMenu = buildFileMenu();
     fileMenu.addMenuListener(new StubMenuListener() {
@@ -577,6 +566,17 @@ public class Editor extends JFrame implements RunnerListener {
 
     menubar.add(buildHelpMenu());
     setJMenuBar(menubar);
+        fileMenu.setMnemonic(trm("File"));
+    public static int trm(String s) {
+          try {
+             if (i18n == null)
+               return s.charAt(0);
+             else
+               return i18n.getString(s).charAt(0);
+           } catch (MissingResourceException e) {
+               return s.charAt(0);
+          }
+  }
   }
 
 
