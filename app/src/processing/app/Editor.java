18e06d25034675d39bc1b78ef6edb6ecc79153fe
shawayusx <ayushshaw456@gmail.com>
2020-03-28 23:36:30 +0530

    reverting back

--- app/src/processing/app/Editor.java
+++ app/src/processing/app/Editor.java
@@ -508,16 +508,6 @@ public class Editor extends JFrame implements RunnerListener {
 
   private void buildMenuBar() {
   fileMenu.setMnemonic(trm("File"));
-  public static int trm(String s) {
-    try {
-      if (i18n == null)
-         return s.charAt(0);
-      else
-         return i18n.getString(s).charAt(0);
-        } catch (MissingResourceException e) {
-               return s.charAt(0);
-          }
-  }
     JMenuBar menubar = new JMenuBar();
     final JMenu fileMenu = buildFileMenu();
     fileMenu.addMenuListener(new StubMenuListener() {
