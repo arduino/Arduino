7883dfe17c80f8ede862eeb8568d6e2235867c61
Fatima-Zahra KERDAD <fatima-zahra.kerdad@imt-atlantique.net>
2020-12-03 01:43:15 +0100

    Ajout de l'icone folder + message de la perspective projet

--- app/src/processing/app/EditorProject.java
+++ app/src/processing/app/EditorProject.java
@@ -3,15 +3,17 @@ package processing.app;
 import processing.app.tools.jexplorer.JExplorerPanel;
 
 import javax.swing.*;
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.DefaultTreeModel;
-import javax.swing.tree.TreePath;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.File;
+import java.io.IOException;
+
+import static java.awt.Color.RED;
+import static processing.app.I18n.tr;
+
 
 public class EditorProject extends Panel implements ActionListener, MouseListener{
 
@@ -35,7 +37,7 @@ public class EditorProject extends Panel implements ActionListener, MouseListene
 
   private Label buttons_popup;
 
-  public EditorProject(String path, Base base, Editor editor){
+  public EditorProject(String path, Base base, Editor editor) throws IOException {
     this.setLayout(new BorderLayout());
     fileRoot = new File(path);
     this.base = base;
@@ -49,18 +51,23 @@ public class EditorProject extends Panel implements ActionListener, MouseListene
     settingsbox.setOpaque(true);
     settingsbox.setBackground(new Color(23, 161, 165));
     this.path = path;
-    settings = new JButton("S");
+
+    Icon img = new ImageIcon(new ImageIcon("folder1.jpg").getImage().getScaledInstance(35, 30, Image.SCALE_DEFAULT));
+    settings = new JButton(img);
+    
+
+    settings.setPreferredSize(new Dimension(25, 5));
     settings.addActionListener(this);
     settings.addMouseListener(this);
     toggle = new JButton("T");
     toggle.addActionListener(this);
     toggle.addMouseListener(this);
-    this.add(settings);
+    //this.add(settings);
     //settingsbox.add(new Label("Project View"));
     JPanel buttons_panel = new JPanel();
     buttons_panel.setLayout(new FlowLayout());
     buttons_panel.setOpaque(false);
-
+    //settingsbox.setBackground(Color.WHITE);
     settingsbox.add(settings);
     settingsbox.add(toggle);
     settingsbox.add(buttons_popup);
@@ -78,6 +85,7 @@ public class EditorProject extends Panel implements ActionListener, MouseListene
 
     // Resetting the sketchbook path
     PreferencesData.set("sketchbook.path", path);
+    editor.statusNotice(tr("Workspace moved to :")+path);
   }
 
 
